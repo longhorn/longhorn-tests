104009bc27fc68e8b54e93c1f0003d9c12307977
Sheng Liang <sheng@rancher.com>
2016-10-28 13:19:01 -0700

    Change to directio

--- stress/teardown-test.py
+++ stress/teardown-test.py
@@ -5,6 +5,8 @@ import sys
 import threading
 import time
 import os
+import directio
+import mmap
 from os import path
 import stat
 
@@ -20,15 +22,54 @@ subprocess.call("docker network create --subnet=172.18.0.0/16 longhorn-net", she
 
 DATA_LEN = 100 * 1024
 
+PAGE_SIZE = 4096
+
+def readat_direct(dev, offset, length):
+    pg = offset / PAGE_SIZE
+    in_page_offset = offset % PAGE_SIZE
+    # either read less than a page, or whole pages
+    if in_page_offset != 0:
+        assert pg == (offset + length - 1) / PAGE_SIZE
+        to_read = PAGE_SIZE
+    else:
+        assert length % PAGE_SIZE == 0
+        to_read = length
+    pg_offset = pg * PAGE_SIZE
+
+    f = os.open(dev, os.O_DIRECT | os.O_RDONLY)
+    try:
+        os.lseek(f, pg_offset, os.SEEK_SET)
+        ret = directio.read(f, to_read)
+    finally:
+        os.close(f)
+    return ret[in_page_offset: in_page_offset + length]
+
+
+def writeat_direct(dev, offset, data):
+    pg = offset / PAGE_SIZE
+    # don't support across page write
+    assert pg == (offset + len(data) - 1) / PAGE_SIZE
+    pg_offset = pg * PAGE_SIZE
+
+    f = os.open(dev, os.O_DIRECT | os.O_RDWR)
+    m = mmap.mmap(-1, PAGE_SIZE)
+    try:
+        os.lseek(f, pg_offset, os.SEEK_SET)
+        pg_data = readat_direct(dev, pg_offset, PAGE_SIZE)
+        m.write(pg_data)
+        m.seek(offset % PAGE_SIZE)
+        m.write(data)
+        ret = directio.write(f, m)
+    finally:
+        m.close()
+        os.close(f)
+    return ret
+
 def write_data(i, pattern):
-  fd = open("/dev/longhorn/vol" + str(i), "r+")
-  fd.write(bytearray([pattern]*DATA_LEN))
-  fd.close()
+  writeat_direct("/dev/longhorn/vol" + str(i), 0, str(chr(pattern))*PAGE_SIZE)
 
 def check_data(i, pattern):
-  fd = open("/dev/longhorn/vol" + str(i), "r")
-  data = fd.read(DATA_LEN)
-  fd.close()
+  data = readat_direct("/dev/longhorn/vol" + str(i), 0, PAGE_SIZE)
   assert ord(data[0]) == pattern
  
 def create_snapshot(controller):
