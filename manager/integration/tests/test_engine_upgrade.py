281d75464b5891c3b422c52eed118d727da0aa4f
Sheng Yang <sheng@yasker.org>
2018-05-25 15:05:19 -0700

    manager: Add engine live upgrade positive test case

--- manager/integration/tests/test_engine_upgrade.py
+++ manager/integration/tests/test_engine_upgrade.py
@@ -7,8 +7,12 @@ from common import wait_for_volume_engine_image
 
 from conftest import engine_upgrade_image  # NOQA
 
+
+REPLICA_COUNT = 2
+
+
 @pytest.mark.engine_upgrade  # NOQA
-def test_engine_upgrade_offline(clients, volume_name,   # NOQA
+def test_engine_offline_upgrade(clients, volume_name,   # NOQA
                                 engine_upgrade_image):  # NOQA
     assert engine_upgrade_image != ""
 
@@ -17,7 +21,7 @@ def test_engine_upgrade_offline(clients, volume_name,   # NOQA
         break
 
     volume = client.create_volume(name=volume_name, size=SIZE,
-                                  numberOfReplicas=2)
+                                  numberOfReplicas=REPLICA_COUNT)
     volume = wait_for_volume_state(client, volume_name, "detached")
 
     assert volume["name"] == volume_name
@@ -51,6 +55,68 @@ def test_engine_upgrade_offline(clients, volume_name,   # NOQA
     assert volume["controller"]["engineImage"] == original_engine_image
     for replica in volume["replicas"]:
         assert replica["engineImage"] == original_engine_image
+
+    client.delete(volume)
+    wait_for_volume_delete(client, volume_name)
+
+
+@pytest.mark.engine_upgrade  # NOQA
+def test_engine_live_upgrade(clients, volume_name,   # NOQA
+                             engine_upgrade_image):  # NOQA
+    assert engine_upgrade_image != ""
+
+    # get a random client
+    for host_id, client in clients.iteritems():
+        break
+
+    volume = client.create_volume(name=volume_name, size=SIZE,
+                                  numberOfReplicas=2)
+    volume = wait_for_volume_state(client, volume_name, "detached")
+
+    assert volume["name"] == volume_name
+
+    original_engine_image = volume["engineImage"]
+    assert original_engine_image != engine_upgrade_image
+
+    volume = volume.attach(hostId=host_id)
+    volume = wait_for_volume_state(client, volume_name, "healthy")
+
+    volume.engineUpgrade(image=engine_upgrade_image)
+    volume = wait_for_volume_engine_image(client, volume_name,
+                                          engine_upgrade_image)
+
+    assert volume["controller"]["engineImage"] == engine_upgrade_image
+    count = 0
+    # old replica may be in deletion process
+    for replica in volume["replicas"]:
+        if replica["engineImage"] == engine_upgrade_image:
+            count += 1
+    assert count == REPLICA_COUNT
+
+    volume = volume.detach()
+    volume = wait_for_volume_state(client, volume_name, "detached")
+    assert len(volume["replicas"]) == REPLICA_COUNT
+
+    volume = volume.attach(hostId=host_id)
+    volume = wait_for_volume_state(client, volume_name, "healthy")
+
+    volume.engineUpgrade(image=original_engine_image)
+    volume = wait_for_volume_engine_image(client, volume_name,
+                                          original_engine_image)
+
+    assert volume["engineImage"] == original_engine_image
+
+    assert volume["controller"]["engineImage"] == original_engine_image
+    count = 0
+    # old replica may be in deletion process
+    for replica in volume["replicas"]:
+        if replica["engineImage"] == original_engine_image:
+            count += 1
+    assert count == REPLICA_COUNT
+
+    volume = volume.detach()
+    volume = wait_for_volume_state(client, volume_name, "detached")
+    assert len(volume["replicas"]) == REPLICA_COUNT
 
     client.delete(volume)
     wait_for_volume_delete(client, volume_name)
