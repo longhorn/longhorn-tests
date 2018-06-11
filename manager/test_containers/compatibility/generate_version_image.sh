4afd0b76b069f58b9ca1e9f6d47a3636ab99614b
Sheng Yang <sheng@yasker.org>
2018-06-11 15:18:44 -0700

    manager: Add compatibility test image builder

---
+++ manager/test_containers/compatibility/generate_version_image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -lt 6 ]
+then
+	echo not enough arguments
+	exit
+fi
+
+export CLIAPIVersion=$1
+export CLIAPIMinVersion=$2
+export ControllerAPIVersion=$3
+export ControllerAPIMinVersion=$4
+export DataFormatVersion=$5
+export DataFormatMinVersion=$6
+
+version_tag="version-test.${CLIAPIVersion}-${CLIAPIMinVersion}"\
+".${ControllerAPIVersion}-${ControllerAPIMinVersion}"\
+".${DataFormatVersion}-${DataFormatMinVersion}"
+
+export Version="\"${version_tag}\""
+
+yaml=`envsubst < template.yaml`
+echo -e "#!/bin/sh\necho '$yaml'" > package/longhorn
+chmod a+x package/longhorn
+
+docker build -t rancher/longhorn-test:${version_tag} package/
+echo
+echo rancher/longhorn-test:${version_tag}
