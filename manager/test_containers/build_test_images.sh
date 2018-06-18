453466e367b0edfbd261f0d8f402d61347562624
Sheng Yang <sheng@yasker.org>
2018-06-18 12:31:22 -0700

    manager: Update version compatibility check with the new engine image format
    Also add live upgrade test image.

--- manager/test_containers/build_test_images.sh
+++ manager/test_containers/build_test_images.sh
@@ -7,6 +7,11 @@ img1=`./generate_version_image.sh 2 2 1 1 1 1 | tail -n 1`
 img2=`./generate_version_image.sh 0 0 1 1 1 1 | tail -n 1`
 img3=`./generate_version_image.sh 1 1 2 2 1 1 | tail -n 1`
 img4=`./generate_version_image.sh 1 1 0 0 1 1 | tail -n 1`
+img5=`./generate_version_image.sh 1 1 1 1 1 1 | tail -n 1`
+
+cd ..
+cd upgrade
+live_upgrade_image=`./generate_live_upgrade_image.sh | tail -n 1`
 
 echo build done
 echo The commands for push:
@@ -14,3 +19,5 @@ echo docker push $img1
 echo docker push $img2
 echo docker push $img3
 echo docker push $img4
+echo docker push $img5
+echo docker push $live_upgrade_image
