825b5d3a97077b5aefabceccd5c92476776f0282
James Oliver <joliver@rancher.com>
2018-07-30 13:09:27 -0700

    add base image ha tests

--- manager/integration/tests/test_base_image.py
+++ manager/integration/tests/test_base_image.py
@@ -2,6 +2,7 @@ from common import clients, volume_name  # NOQA
 from common import BASE_IMAGE
 from test_basic import volume_basic_test, volume_iscsi_basic_test
 from test_basic import snapshot_test, backup_test
+from test_ha import ha_simple_recovery_test, ha_salvage_test
 
 
 def test_volume_basic_with_base_image(clients, volume_name):  # NOQA
@@ -18,3 +19,11 @@ def test_snapshot_with_base_image(clients, volume_name):  # NOQA
 
 def test_backup_with_base_image(clients, volume_name):  # NOQA
     backup_test(clients, volume_name, BASE_IMAGE)
+
+
+def test_ha_simple_recovery_with_base_image(client, volume_name):  # NOQA
+    ha_simple_recovery_test(client, volume_name, BASE_IMAGE)
+
+
+def test_ha_salvage_with_base_image(client, volume_name):  # NOQA
+    ha_salvage_test(client, volume_name, BASE_IMAGE)
