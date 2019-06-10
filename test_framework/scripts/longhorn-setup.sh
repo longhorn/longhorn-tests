44b1c7ed72cb0134c78fc2bfbbb5b0da7a9f3547
meldafrawi <mohamed.eldafrawi@rancher.com>
2019-06-11 00:33:35 +0200

    nightly test: Fix longhorn-setup.sh to rely on default longhorn.yaml

--- test_framework/scripts/longhorn-setup.sh
+++ test_framework/scripts/longhorn-setup.sh
@@ -2,25 +2,29 @@
 
 set  -x
 
-LONGHORN_MANAGER_IMAGE_VERSION="v0.5.0"
-LONGHORN_ENGINE_IMAGE_VERSION="v0.5.0"
+wget https://raw.githubusercontent.com/rancher/longhorn/master/deploy/longhorn.yaml 
 
-sed -i 's/LONGHORN_MANAGER_TAG/'${LONGHORN_MANAGER_IMAGE_VERSION}'/' "${TF_VAR_tf_workspace}/templates/longhorn.yaml"
+LONGHORN_MANAGER_IMAGE_TAG=`grep -i "rancher\/longhorn-manager:.*$" longhorn.yaml | head -1 | awk -F ":" '{print $2}'`
+LONGHORN_ENGINE_IMAGE_TAG=`grep -i "rancher\/longhorn-engine:.*$" longhorn.yaml | head -1 | awk -F ":" '{print $2}'
 
-sed -i 's/LONGHORN_ENGINE_TAG/'${LONGHORN_ENGINE_IMAGE_VERSION}'/'   "${TF_VAR_tf_workspace}/templates/longhorn.yaml"
+
+sed -i 's/rancher\/longhorn-manager:'${LONGHORN_MANAGER_IMAGE_TAG}'/rancher\/longhorn-manager:v0.5.0' longhorn.yaml
+sed -i 's/rancher\/longhorn-engine:'${LONGHORN_ENGINE_IMAGE_TAG}'/rancher\/longhorn-engine:v0.5.0' longhorn.yaml
 
 export KUBECONFIG="${TF_VAR_tf_workspace}/templates/kube_config_3-nodes-k8s.yml"
 
-kubectl apply -f "${TF_VAR_tf_workspace}/templates/longhorn.yaml"
+kubectl apply -f longhorn.yaml
 
 while [[ -n "`kubectl get pods -n longhorn-system  | grep "longhorn-\(manager\|driver\)\|engine-image-.*" | awk '{print $3}' | grep -v Running`"  ]]; do
-  echo "Longhorn is installing ... rechecking in 1m"
+  echo "Longhorn is being inatalled ... rechecking in 1m"
   sleep 1m
 done
 
-kubectl create -Rf manager/integration/deploy/backupstores
+kubectl create -Rf "${WORKSPACE}/manager/integration/deploy/backupstores"
 
-kubectl create -f manager/integration/deploy/test.yaml
+sedi -i 's/#TEST_FRAMEWORK_ARGS_PLACEHOLDER/args:\ \[\ \"\-s\"\ ,\ \"\-\-junitxml=\$\{LONGHORN_JUNIT_REPORT_PATH\}" \]/' "${WORKSPACE}/manager/integration/deploy/test.yaml"
+
+kubectl create -f "${WORKSPACE}/manager/integration/deploy/test.yaml"
 
 echo "Waiting for test pod to be in Running state"
 sleep 5m
