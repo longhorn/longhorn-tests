e30dacea13dbafe72c0d55b9f202dba31a751aa5
meldafrawi <mohamed.eldafrawi@rancher.com>
2019-07-03 01:45:32 +0200

    Add output message to flake8-check.sh

--- manager/integration/scripts/flake8-check.sh
+++ manager/integration/scripts/flake8-check.sh
@@ -1,7 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 
-source manager/integration/.venv/bin/activate 
+source manager/integration/.venv/bin/activate
 
 pip install -r manager/integration/tests/requirements.txt
 
 flake8 manager/integration/tests
+
+if [[ $? -eq 0 ]] ; then
+    echo "flake8 check succeed"
+    exit 0
+else
+    echo "flake8 check failed"
+    exit 1
+fi
