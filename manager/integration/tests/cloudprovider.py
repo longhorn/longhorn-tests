dc16a6553a939579caaee48e59c0a2cf3658ab9c
meldafrawi <mohamed.eldafrawi@rancher.com>
2019-07-26 02:31:05 +0200

    Add cloud provider API and digital ocean implementation

---
+++ manager/integration/tests/cloudprovider.py
@@ -0,0 +1,13 @@
+class cloudprovider:
+
+    def node_id(self):
+        pass
+
+    def node_shutdown(self):
+        pass
+
+    def node_start(self):
+        pass
+
+    def node_status(self):
+        pass
