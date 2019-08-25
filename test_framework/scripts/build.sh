3dd07a8968b5e2094c31edce68b3c28a30a472c5
meldafrawi <mohamed.eldafrawi@rancher.com>
2019-08-25 19:43:53 +0200

    test framework: use --no-cache in build script
    Signed-off-by: meldafrawi <mohamed.eldafrawi@rancher.com>

--- test_framework/scripts/build.sh
+++ test_framework/scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -f ./test_framework/Dockerfile.setup -t "${JOB_NAME}${BUILD_NUMBER}" .
+docker build --no-cache -f ./test_framework/Dockerfile.setup -t "${JOB_NAME}${BUILD_NUMBER}" .
