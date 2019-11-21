463a67065a915937cf186b5bcdb1e72facbd69fe
Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>
2019-11-21 03:04:14 +0200

    add upgrade test to nighly test
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>

--- test_framework/scripts/longhorn-setup.sh
+++ test_framework/scripts/longhorn-setup.sh
@@ -7,6 +7,23 @@ RETRY_COUNTS=10
 LONGHORN_MANAGER_REPO_URI="https://github.com/longhorn/longhorn-manager.git"
 LONGHORN_MANAGER_BRANCH="master"
 LONGHORN_MANAGER_TMPDIR="/tmp/longhorn-manager"
+LONGHORN_STABLE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml"
+
+
+check_longhorn_status() {
+  export KUBECONFIG="${TF_VAR_tf_workspace}/templates/kube_config_3-nodes-k8s.yml"
+
+  RETRIES=0
+  while [[ -n "`kubectl get pods -n longhorn-system  | grep "instance-manager-.*\|longhorn-\(manager\|driver\|csi\)\|engine-image-.*" | awk '{print $3}' | grep -v Running`"  ]]; do
+    echo "Longhorn is being installed ... rechecking in 1m"
+    sleep 1m
+    RETRIES=$((RETRIES+1))
+
+    if [[ ${RETRIES} -eq ${RETRY_COUNTS} ]]; then echo "Error: longhorn installation timeout"; exit 1 ; fi
+  done
+
+}
+
 
 mkdir -p ${LONGHORN_MANAGER_TMPDIR}
 
@@ -31,20 +48,20 @@ export KUBECONFIG="${TF_VAR_tf_workspace}/templates/kube_config_3-nodes-k8s.yml"
 # scale coredns min pods to 3 for node offline tests
 kubectl get configmaps -n kube-system coredns-autoscaler -o yaml | sed  's/\"min\":1/\"min\":3/' | kubectl apply -n kube-system -f -
 
-kubectl apply -f longhorn.yaml
+if [[ ${LONGHORN_UPGRADE_TEST} ]]; then
+  ## install Longhorn stable version, before running test_upgrade.
+  kubectl apply -f "${LONGHORN_STABLE_URL}"
+  check_longhorn_status
+  sed -i 's/#TEST_FRAMEWORK_ARGS_PLACEHOLDER/args:\ \[\ \"\-s\"\ ,\ \"\-\-junitxml=\$\{LONGHORN_JUNIT_REPORT_PATH\}",\ \"\-\-include\-upgrade\-test\ \-k test_upgrade\" \]/' "${WORKSPACE}/manager/integration/deploy/test.yaml"
 
-RETRIES=0
-while [[ -n "`kubectl get pods -n longhorn-system  | grep "instance-manager-.*\|longhorn-\(manager\|driver\|csi\)\|engine-image-.*" | awk '{print $3}' | grep -v Running`"  ]]; do
-  echo "Longhorn is being installed ... rechecking in 1m"
-  sleep 1m
-  RETRIES=$((RETRIES+1))
+else
+  kubectl apply -f longhorn.yaml
+  check_longhorn_status
 
-  if [[ ${RETRIES} -eq ${RETRY_COUNTS} ]]; then echo "Error: longhorn installation timeout"; exit 1 ; fi
-done
+  sed -i 's/#TEST_FRAMEWORK_ARGS_PLACEHOLDER/args:\ \[\ \"\-s\"\ ,\ \"\-\-junitxml=\$\{LONGHORN_JUNIT_REPORT_PATH\}" \]/' "${WORKSPACE}/manager/integration/deploy/test.yaml"
+fi
 
 kubectl create -Rf "${WORKSPACE}/manager/integration/deploy/backupstores"
-
-sed -i 's/#TEST_FRAMEWORK_ARGS_PLACEHOLDER/args:\ \[\ \"\-s\"\ ,\ \"\-\-junitxml=\$\{LONGHORN_JUNIT_REPORT_PATH\}" \]/' "${WORKSPACE}/manager/integration/deploy/test.yaml"
 
 sed  -i 's/longhornio\/longhorn-manager-test:.*$/longhornio\/longhorn-manager-test:master/' "${WORKSPACE}/manager/integration/deploy/test.yaml"
 
