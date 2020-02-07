23395cde6119e1988235fdbf3fac82bda92da920
Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>
2020-02-07 02:03:17 +0200

    add k8s zone tag integration test
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>

--- manager/integration/tests/common.py
+++ manager/integration/tests/common.py
@@ -164,6 +164,11 @@ def get_storage_api_client():
     return k8sclient.StorageV1Api()
 
 
+def get_version_api_client():
+    load_k8s_config()
+    return k8sclient.VersionApi()
+
+
 def get_longhorn_api_client():
     for i in range(RETRY_COUNTS):
         try:
