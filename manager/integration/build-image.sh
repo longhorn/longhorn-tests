dc53833f978987fcf06e31074948d92e9ec28415
Bo Tao <bo.tao@rancher.com>
2020-10-13 12:56:21 -0700

    Add arm64 support
    Signed-off-by: Bo Tao <bo.tao@rancher.com>

--- manager/integration/build-image.sh
+++ manager/integration/build-image.sh
@@ -4,7 +4,21 @@ REPO=${REPO:-longhornio}
 
 version=$(./scripts/version)
 image="$REPO/longhorn-manager-test:${version}"
-docker build -t ${image} .
+
+case $(uname -m) in
+        aarch64 | arm64)
+                ARCH=arm64
+                ;;
+        x86_64)
+                ARCH=amd64
+                ;;
+        *)
+                echo "$(uname -a): unsupported architecture"
+                exit 1
+esac
+
+echo "Building for ${ARCH}"
+docker build --build-arg ARCH=${ARCH} -t ${image} .
 mkdir -p bin
 echo ${image} > bin/latest_image
 echo Built image ${image}
