8f0a3def6592cc70fcd8dec5d028ac25390aa33a
Sheng Yang <sheng.yang@rancher.com>
2020-12-16 18:54:28 -0800

    Add longhorn-share-manager to security scan
    Signed-off-by: Sheng Yang <sheng.yang@rancher.com>

--- secscan/scripts/secscan.sh
+++ secscan/scripts/secscan.sh
@@ -5,7 +5,7 @@ SEVERITY=${1}
 mkdir -p /junit-reports /templates
 
 REPO="longhornio"
-IMAGES=("longhorn-manager" "longhorn-engine" "longhorn-instance-manager" "longhorn-ui")
+IMAGES=("longhorn-manager" "longhorn-engine" "longhorn-instance-manager" "longhorn-ui" "longhorn-share-manager")
 TAG="master"
 
 wget -O /templates/junit.tpl https://raw.githubusercontent.com/longhorn/longhorn-tests/master/secscan/templates/junit.tpl
