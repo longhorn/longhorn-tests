d2bca212f243135092f286fa0d0edb02c67d66b6
Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>
2021-01-27 22:16:43 +0200

    add RHEL support to test framework
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>

--- test_framework/scripts/terraform-setup.sh
+++ test_framework/scripts/terraform-setup.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-terraform init  ${TF_VAR_tf_workspace}/terraform/aws
-terraform apply -auto-approve -no-color ${TF_VAR_tf_workspace}/terraform/aws
+terraform init  ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
+terraform apply -auto-approve -no-color ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
 
 if [[ ${TF_VAR_arch} == "amd64" ]]; then
-	terraform refresh ${TF_VAR_tf_workspace}/terraform/aws
+	terraform refresh ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
 	terraform output rke_config > ${TF_VAR_tf_workspace}/rke.yml
 	rke up --config ${TF_VAR_tf_workspace}/rke.yml
 fi
