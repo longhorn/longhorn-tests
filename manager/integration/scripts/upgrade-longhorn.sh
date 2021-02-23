a8200513ada4af2f568d13693485542f18a428ef
Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>
2021-02-23 17:17:07 +0200

    test framework: refactor longhorn setup script
    - add support for using custom longhorn images for running nightly integration tests
    - add support for running specefic tests, or group of tests
    - add support for longhorn upgrade using different longhorn versions
    
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>

--- manager/integration/scripts/upgrade-longhorn.sh
+++ manager/integration/scripts/upgrade-longhorn.sh
@@ -1,41 +1,51 @@
 #!/bin/bash 
 
-LONGHORN_MANAGER_REPO_URI="https://github.com/longhorn/longhorn-manager.git"
-LONGHORN_MANAGER_BRANCH="master"
-LONGHORN_MANAGER_TMPDIR="/tmp/longhorn-manager"
+LONGHORN_MANAGER_REPO_URI="${1}"
+LONGHORN_MANAGER_BRANCH="${2}"
+CUSTOM_LONGHORN_MANAGER_IMAGE="${3}"
+CUSTOM_LONGHORN_INSTANCE_MANAGER_IMAGE="${4}"
+CUSTOM_LONGHORN_SHARE_MANAGER_IMAGE="${5}"
+CUSTOM_LONGHORN_ENGINE_IMAGE="${6}"
 
-LONGHORN_MANIFEST="/tmp/longhorn-master.yml"
+LONGHORN_MANAGER_REPO_DIR="/tmp/longhorn-manager"
+LONGHORN_MANIFEST="/tmp/longhorn.yml"
 
-UPGRADE_IMAGE_TAG=${1:-"master"}
+LONGHORN_NAMESPACE="longhorn-system"
 
-check_longhorn_status() {
-  RETRIES=0
-  while [[ -n "`kubectl get pods -n longhorn-system  | grep "instance-manager-.*\|longhorn-\(manager\|driver\|csi\)\|engine-image-.*" | awk '{print $3}' | grep -v Running`"  ]]; do
-    sleep 1m
-    RETRIES=$((RETRIES+1))
-
-    if [[ ${RETRIES} -eq ${RETRY_COUNTS} ]]; then echo "Error: longhorn installation timeout"; exit 1 ; fi
-  done
-
+wait_longhorn_status_running(){                                                 
+    local RETRY_COUNTS=10  # in minutes                                         
+    local RETRY_INTERVAL="1m"                                                   
+                                                                                
+    RETRIES=0                                                                   
+    while [[ -n `kubectl get pods -n ${LONGHORN_NAMESPACE} --no-headers | awk '{print $3}' | grep -v Running` ]]; do
+        echo "Longhorn is still installing ... re-checking in 1m"               
+        sleep ${RETRY_INTERVAL}                                                 
+        RETRIES=$((RETRIES+1))                                                  
+                                                                                
+        if [[ ${RETRIES} -eq ${RETRY_COUNTS} ]]; then echo "Error: longhorn installation timeout"; exit 1 ; fi
+    done                                                                        
 }
 
+git clone --single-branch \
+          --branch ${LONGHORN_MANAGER_BRANCH} \
+          ${LONGHORN_MANAGER_REPO_URI} \
+          ${LONGHORN_MANAGER_REPO_DIR}
 
-mkdir -p ${LONGHORN_MANAGER_TMPDIR}
-
-git clone --quiet --single-branch --branch ${LONGHORN_MANAGER_BRANCH} ${LONGHORN_MANAGER_REPO_URI} ${LONGHORN_MANAGER_TMPDIR}
-
-for FILE in `find "${LONGHORN_MANAGER_TMPDIR}/deploy/install" -type f -name "*\.yaml" | sort`; do
-    cat "${FILE}" >> "${LONGHORN_MANIFEST}"
-    echo "---"    >> "${LONGHORN_MANIFEST}"
+for FILE in `find "${LONGHORN_MANAGER_REPO_DIR}/deploy/install" -type f -name "*\.yaml" | sort`; do
+  cat ${FILE} >> "${LONGHORN_MANIFEST}"
+  echo "---"  >> "${LONGHORN_MANIFEST}"
 done
 
-LONGHORN_MANAGER_IMAGE_TAG=`grep -io "longhornio\/longhorn-manager:.*$" "${LONGHORN_MANIFEST}" | head -1 | awk -F ":" '{print $2}'`
-LONGHORN_ENGINE_IMAGE_TAG=`grep -io "longhornio\/longhorn-engine:.*$" "${LONGHORN_MANIFEST}" | head -1 | awk -F ":" '{print $2}'`
+LONGHORN_MANAGER_IMAGE=`grep -io "longhornio\/longhorn-manager:.*$" "${LONGHORN_MANIFEST}"| head -1`
+LONGHORN_ENGINE_IMAGE=`grep -io "longhornio\/longhorn-engine:.*$" "${LONGHORN_MANIFEST}"| head -1`
+LONGHORN_INSTANCE_MANAGER_IMAGE=`grep -io "longhornio\/longhorn-instance-manager:.*$" "${LONGHORN_MANIFEST}"| head -1`
+LONGHORN_SHARE_MANAGER_IMAGE=`grep -io "longhornio\/longhorn-share-manager:.*$" "${LONGHORN_MANIFEST}"| head -1`
 
-
-sed -i 's/longhornio\/longhorn-manager:'${LONGHORN_MANAGER_IMAGE_TAG}'/longhornio\/longhorn-manager:'${UPGRADE_IMAGE_TAG}'/' "${LONGHORN_MANIFEST}"
-sed -i 's/longhornio\/longhorn-engine:'${LONGHORN_ENGINE_IMAGE_TAG}'/longhornio\/longhorn-engine:'${UPGRADE_IMAGE_TAG}'/' "${LONGHORN_MANIFEST}"
+# replace longhorn images with custom images
+sed -i 's#'${LONGHORN_MANAGER_IMAGE}'#'${CUSTOM_LONGHORN_MANAGER_IMAGE}'#' "${MANIFEST_BASEDIR}/longhorn.yaml"
+sed -i 's#'${LONGHORN_ENGINE_IMAGE}'#'${CUSTOM_LONGHORN_ENGINE_IMAGE}'#' "${MANIFEST_BASEDIR}/longhorn.yaml"
+sed -i 's#'${LONGHORN_INSTANCE_MANAGER_IMAGE}'#'${CUSTOM_LONGHORN_INSTANCE_MANAGER_IMAGE}'#' "${MANIFEST_BASEDIR}/longhorn.yaml"
+sed -i 's#'${LONGHORN_SHARE_MANAGER_IMAGE}'#'${CUSTOM_LONGHORN_SHARE_MANAGER_IMAGE}'#' "${MANIFEST_BASEDIR}/longhorn.yaml"
 
 kubectl apply -f "${LONGHORN_MANIFEST}" > /dev/null
-
-check_longhorn_status
+wait_longhorn_status_running
