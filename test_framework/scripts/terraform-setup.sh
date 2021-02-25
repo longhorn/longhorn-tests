a8c84558e84181b8e25db7235bba962b12f2a636
Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>
2021-02-25 23:23:58 +0200

    fix terraform infinite wait in case of error
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>

--- test_framework/scripts/terraform-setup.sh
+++ test_framework/scripts/terraform-setup.sh
@@ -2,11 +2,15 @@
 
 set -x
 
-terraform init  ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
-terraform apply -auto-approve -no-color ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
-
 if [[ ${TF_VAR_arch} == "amd64" ]]; then
+	terraform init  ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
+	terraform apply -auto-approve -no-color ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
 	terraform refresh ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
 	terraform output rke_config > ${TF_VAR_tf_workspace}/rke.yml
 	rke up --config ${TF_VAR_tf_workspace}/rke.yml
+	exit $?
+else
+	terraform init  ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
+	terraform apply -auto-approve -no-color ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
+	exit $?
 fi
