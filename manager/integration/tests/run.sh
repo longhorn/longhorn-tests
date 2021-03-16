a0305c56f60de7ce6f3e532ec4e8fc563c92edf8
Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>
2021-03-16 20:51:12 +0200

    modify run.sh script to have the ability to run custom tests
    the will still run all tests by default
    
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>

--- manager/integration/tests/run.sh
+++ manager/integration/tests/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $@ =~ "--junitxml=" ]] ; then 
-  flake8 .
+  flake8
 
-  py.test -v . $@  >> /tmp/longhorn-pytest 2>&1
+  pytest -v $@  >> /tmp/longhorn-pytest 2>&1
   
   cat ${LONGHORN_JUNIT_REPORT_PATH}
 else 
   set -x 
   
-  flake8 .
+  flake8
 
-  py.test -v . $@
+  pytest -v $@
 fi 
