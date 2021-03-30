67552d1ff54136cccf35051af85f002594cfadbc
Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>
2021-03-30 21:56:07 +0200

    wait 30 seconds for docker to completetly start
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@rancher.com>

--- test_framework/scripts/terraform-setup.sh
+++ test_framework/scripts/terraform-setup.sh
@@ -7,6 +7,7 @@ if [[ ${TF_VAR_arch} == "amd64" ]]; then
 	terraform apply -auto-approve -no-color ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
 	terraform refresh ${TF_VAR_tf_workspace}/terraform/aws/${DISTRO}
 	terraform output rke_config > ${TF_VAR_tf_workspace}/rke.yml
+	sleep 30
 	rke up --config ${TF_VAR_tf_workspace}/rke.yml
 	exit $?
 else
