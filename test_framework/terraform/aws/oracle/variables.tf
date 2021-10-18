9fad959c5952a1f9701e7cb024aa95d789070f46
Mohamed Eldafrawi <mohamed.eldafrawi@suse.com>
2021-10-18 22:24:21 +0200

    add oracle linux support to nightly tests
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@suse.com>

---
+++ test_framework/terraform/aws/oracle/variables.tf
@@ -0,0 +1,102 @@
+variable "lh_aws_access_key" {
+  type        = string
+  description = "AWS ACCESS_KEY"
+}
+
+variable "lh_aws_secret_key" {
+  type        = string
+  description = "AWS SECRET_KEY"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "aws_availability_zone" {
+  type        = string
+  default     = "us-east-2a"
+}
+
+variable "lh_aws_vpc_name" {
+  type        = string
+  default     = "vpc-longhorn-tests-meldafrawi"
+}
+
+variable "arch" {
+  type        = string
+  description = "available values (amd64, arm64)"
+  default = "amd64"
+}
+
+variable "distro_version" {
+  type        = string
+  default     = "8.3"
+}
+
+variable "aws_ami_oraclelinux_account_number" {
+  type        = string
+  default     = "131827586825"
+}
+
+variable "lh_aws_instance_count_controlplane" {
+  type        = number
+  default     = 1
+}
+
+variable "lh_aws_instance_count_worker" {
+  type        = number
+  default     = 3
+}
+
+variable "lh_aws_instance_name_controlplane" {
+  type        = string
+  default     = "lh-tests-controlplane"
+}
+
+variable "lh_aws_instance_type_controlplane" {
+  type        = string
+  description = "Recommended instance types t2.xlarge for amd64 & a1.xlarge  for arm64"
+}
+
+variable "lh_aws_instance_type_worker" {
+  type        = string
+  description = "Recommended instance types t2.xlarge for amd64 & a1.xlarge  for arm64"
+}
+
+variable "lh_aws_instance_root_block_device_size_controlplane" {
+  type        = number
+  default     = 40
+}
+
+variable "aws_ssh_public_key_file_path" {
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "aws_ssh_private_key_file_path" {
+  type        = string
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "lh_aws_instance_name_worker" {
+  type        = string
+  default     = "lh-tests-worker"
+}
+
+variable "lh_aws_instance_root_block_device_size_worker" {
+  type        = number
+  default     = 40
+}
+
+variable "rke_k8s_version" {
+  type        = string
+  default     = "v1.20.8-rancher1-1"
+  description = "RKE k8s version will be used to generate RKE config file output in case of arch=amd64"
+}
+
+variable "k3s_version" {
+  type        = string
+  default     = "v1.20.8+k3s1"
+  description = "K3s version that will be deployed in case of arch=arm64"
+}
