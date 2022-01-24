9e4b6b0fa5ebf4c93028175218cf07cf85c10249
Mohamed Eldafrawi <mohamed.eldafrawi@suse.com>
2022-01-24 18:24:16 +0200

    fix rke run command when k8s_distro_name is not rke
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@suse.com>

--- test_framework/scripts/terraform-setup.sh
+++ test_framework/scripts/terraform-setup.sh
@@ -5,10 +5,13 @@ set -x
 if [[ ${TF_VAR_arch} == "amd64" ]]; then
 	terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} init
 	terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} apply -auto-approve -no-color
-    terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} apply -auto-approve -no-color -refresh-only
-	terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} output -raw rke_config > ${TF_VAR_tf_workspace}/rke.yml
-	sleep 30
-	rke up --config ${TF_VAR_tf_workspace}/rke.yml
+	
+	if [[ ${TF_VAR_k8s_distro_name} =~ [rR][kK][eE] ]]; then
+    	terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} apply -auto-approve -no-color -refresh-only
+		terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} output -raw rke_config > ${TF_VAR_tf_workspace}/rke.yml
+		sleep 30
+		rke up --config ${TF_VAR_tf_workspace}/rke.yml
+	fi
 else
 	terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} init
 	terraform -chdir=${TF_VAR_tf_workspace}/terraform/aws/${DISTRO} apply -auto-approve -no-color
