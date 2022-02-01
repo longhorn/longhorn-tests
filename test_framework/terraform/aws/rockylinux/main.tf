a37518e803c7f95c18a34d371e97222affcea50e
Mohamed Eldafrawi <mohamed.eldafrawi@suse.com>
2022-02-02 00:02:05 +0200

    fix wait worker nodes condition for k3s
    Signed-off-by: Mohamed Eldafrawi <mohamed.eldafrawi@suse.com>

--- test_framework/terraform/aws/rockylinux/main.tf
+++ test_framework/terraform/aws/rockylinux/main.tf
@@ -382,7 +382,7 @@ resource "null_resource" "rsync_kubeconfig_file" {
   ]
 
   provisioner "remote-exec" {
-    inline = var.k8s_distro_name == "k3s" ? ["until([ -f /etc/rancher/k3s/k3s.yaml ] && [ `sudo /usr/local/bin/kubectl get node -o jsonpath='{.items[*].status.conditions}'  | jq '.[] | select(.type  == \"Ready\").status' | grep -cvi true` -eq 0 ]); do echo \"waiting for k3s cluster nodes to be running\"; sleep 2; done"] : null
+    inline = var.k8s_distro_name == "k3s" ? ["until([ -f /etc/rancher/k3s/k3s.yaml ] && [ `sudo /usr/local/bin/kubectl get node -o jsonpath='{.items[*].status.conditions}'  | jq '.[] | select(.type  == \"Ready\").status' | grep -ci true` -eq 4 ]); do echo \"waiting for k3s cluster nodes to be running\"; sleep 2; done"] : null
 
 
     connection {
