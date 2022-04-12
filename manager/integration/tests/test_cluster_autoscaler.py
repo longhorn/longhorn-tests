89406319d4bec75c6172484695dcd03f9888a4ba
Chin-Ya Huang <chin-ya.huang@suse.com>
2022-04-12 15:13:43 +0800

    test(cluster-autoscaler): scale down nodes with replicas
    Ref: 2203
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- manager/integration/tests/test_cluster_autoscaler.py
+++ manager/integration/tests/test_cluster_autoscaler.py
@@ -8,19 +8,35 @@ from common import apps_api  # NOQA
 from common import client  # NOQA
 from common import core_api  # NOQA
 from common import make_deployment_cpu_request  # NOQA
+from common import pod_make  # NOQA
+from common import volume_name  # NOQA
 
 from common import get_longhorn_api_client
 from common import get_self_host_id
 from common import update_setting
 
+from common import create_and_check_volume
 from common import create_and_wait_deployment
-from test_node import set_node_cordon
+from common import create_and_wait_pod
+from common import create_pv_for_volume
+from common import create_pvc_for_volume
+from common import create_pvc_spec
+from common import delete_and_wait_pod
+from common import generate_random_data
+from common import read_volume_data
+from common import set_node_cordon
+from common import wait_for_volume_detached
+from common import wait_for_volume_healthy
+from common import write_pod_volume_data
+
+from common import Ki
 
 from common import K8S_CLUSTER_AUTOSCALER_EVICT_KEY
 from common import K8S_CLUSTER_AUTOSCALER_SCALE_DOWN_DISABLED_KEY
 from common import RETRY_AUTOSCALER_COUNTS
 from common import RETRY_AUTOSCALER_INTERVAL
 from common import SETTING_K8S_CLUSTER_AUTOSCALER_ENABLED
+from common import SETTING_REPLICA_REPLENISHMENT_WAIT_INTERVAL
 
 CPU_REQUEST = 150
 
@@ -82,6 +98,116 @@ def test_cluster_autoscaler(client, core_api, apps_api, make_deployment_cpu_requ
     client = wait_cluster_autoscale_down(client, core_api, nodes, scale_size)
 
 
+@pytest.mark.skip(reason="require K8s cluster-autoscaler")
+@pytest.mark.cluster_autoscaler  # NOQA
+def test_cluster_autoscaler_all_nodes_with_volume_replicas(client, core_api, apps_api, make_deployment_cpu_request, volume_name, pod_make, request):  # NOQA
+    """
+    Scenario: Test CA scale down all nodes with volume replicas
+
+    Given Cluster with Kubernetes cluster-autoscaler.
+    And Longhorn installed.
+    And Set `kubernetes-cluster-autoscaler-enabled` to `true`.
+    And Create volume.
+    And Attach the volume.
+    And Write some data to volume.
+    And Detach the volume.
+    And Create deployment with cpu request.
+
+    When Trigger CA to scale-up by increase deployment replicas.
+         (double the node number, not including host node)
+    Then Cluster should have double the node number.
+
+    When Annotate new nodes with
+         `cluster-autoscaler.kubernetes.io/scale-down-disabled`.
+         (this ensures scale-down only the old nodes)
+    And Trigger CA to scale-down by decrease deployment replicas.
+    Then Cluster should have original node number + 1 blocked node.
+
+    When Attach the volume to a new node. This triggers replica rebuild.
+    And Volume data should be the same.
+    And Detach the volume.
+    Then Cluster should scale-down to original node number.
+    And Volume data should be the same.
+    """
+    # Cleanup
+    def finalizer():
+        configure_node_scale_down(core_api, client.list_node(), disable="")
+    request.addfinalizer(finalizer)
+
+    host_id = get_self_host_id()
+    host_node = client.by_id_node(host_id)
+    configure_node_scale_down(core_api, [host_node], disable="true")
+    set_node_cordon(core_api, host_id, True)
+
+    update_setting(client, SETTING_REPLICA_REPLENISHMENT_WAIT_INTERVAL, "0")
+    update_setting(client, SETTING_K8S_CLUSTER_AUTOSCALER_ENABLED, "true")
+
+    nodes = client.list_node()
+    scale_size = len(nodes)-1
+
+    volume = create_and_check_volume(
+        client, volume_name, num_of_replicas=scale_size
+    )
+    create_pv_for_volume(client, core_api, volume, volume.name)
+    create_pvc_for_volume(client, core_api, volume, volume.name)
+
+    pod_manifest = pod_make()
+    pod_manifest['spec']['volumes'] = [create_pvc_spec(volume.name)]
+    pod_name = pod_manifest['metadata']['name']
+    create_and_wait_pod(core_api, pod_manifest)
+
+    data = generate_random_data(16*Ki)
+    write_pod_volume_data(core_api, pod_name, data)
+    delete_and_wait_pod(core_api, pod_name)
+    volume = wait_for_volume_detached(client, volume.name)
+
+    scale_up_replica = get_replica_count_to_scale_up(
+        core_api, scale_size, CPU_REQUEST
+    )
+
+    deployment_name = "autoscale-control"
+    deployment = make_deployment_cpu_request(deployment_name, CPU_REQUEST)
+    create_and_wait_deployment(apps_api, deployment)
+
+    deployment["spec"]["replicas"] = scale_up_replica
+    apps_api.patch_namespaced_deployment(body=deployment,
+                                         namespace='default',
+                                         name=deployment_name)
+
+    wait_cluster_autoscale_up(client, nodes, scale_size)
+
+    new_nodes = get_new_nodes(client, old_nodes=nodes)
+    configure_node_scale_down(core_api, new_nodes, disable="true")
+
+    scale_down_replica = 0
+    deployment["spec"]["replicas"] = scale_down_replica
+    apps_api.patch_namespaced_deployment(body=deployment,
+                                         namespace='default',
+                                         name=deployment_name)
+    nodes = client.list_node()
+    is_blocked = False
+    try:
+        client = wait_cluster_autoscale_down(client, core_api, nodes,
+                                             scale_size)
+    except AssertionError:
+        client = wait_cluster_autoscale_down(client, core_api, nodes,
+                                             scale_size-1)
+        is_blocked = True
+    assert is_blocked
+
+    configure_node_scale_down(core_api, [new_nodes[0]], disable="")
+
+    volume = client.by_id_volume(volume.name)
+    volume = volume.attach(hostId=new_nodes[0].id)
+    volume = wait_for_volume_healthy(client, volume_name)
+    volume.detach(hostId="")
+
+    client = wait_cluster_autoscale_down(client, core_api, nodes, scale_size)
+    create_and_wait_pod(core_api, pod_manifest)
+    resp = read_volume_data(core_api, pod_name)
+    assert resp == data
+
+
 def configure_node_scale_down(core_api, nodes, disable):  # NOQA
     payload = {
         "metadata": {
@@ -92,6 +218,12 @@ def configure_node_scale_down(core_api, nodes, disable):  # NOQA
     }
     for node in nodes:
         core_api.patch_node(node.id, body=payload)
+
+
+def get_new_nodes(client, old_nodes):  # NOQA
+    old_nodes_name = [n.name for n in old_nodes]
+    nodes = client.list_node()
+    return [n for n in nodes if not (n.name in old_nodes_name)]
 
 
 def annotate_safe_to_evict_to_namespace_pods(core_api, namespace):  # NOQA
