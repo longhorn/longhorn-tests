47f4db9212c24dac6f454b8f2216353a74323c9f
Chris Chien <chris.chien@suse.com>
2022-09-30 11:40:06 +0800

    change aws_availability_zone from us-east-1b to us-east-1c
    Signed-off-by: Chris Chien <chris.chien@suse.com>

--- build_engine_test_images/terraform/aws/ubuntu/variables.tf
+++ build_engine_test_images/terraform/aws/ubuntu/variables.tf
@@ -21,7 +21,7 @@ variable "tf_workspace" {
 
 variable "aws_availability_zone" {
   type        = string
-  default     = "us-east-1b"
+  default     = "us-east-1c"
 }
 
 variable "build_engine_aws_vpc_name" {
