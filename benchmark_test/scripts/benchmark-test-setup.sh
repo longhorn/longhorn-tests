05c9ed309788f0680decb0a45c50b8544fe732d4
Yang Chiu <yang.chiu@suse.com>
2022-11-15 14:21:35 +0800

    test: running different versions of longhorn in the same benchmark test build for comparison
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- benchmark_test/scripts/benchmark-test-setup.sh
+++ benchmark_test/scripts/benchmark-test-setup.sh
@@ -118,6 +118,21 @@ wait_longhorn_status_running(){
 }
 
 
+wait_longhorn_resources_deleted(){
+  local RETRY_COUNTS=10  # in seconds
+  local RETRY_INTERVAL="10s"
+
+  RETRIES=0
+  while [[ `kubectl get all --no-headers -n longhorn-system | wc -l` -ne 0 ]]; do
+    echo "wait for resources deleted ... re-checking in 10s"
+    sleep ${RETRY_INTERVAL}
+    RETRIES=$((RETRIES+1))
+
+    if [[ ${RETRIES} -eq ${RETRY_COUNTS} ]]; then echo "Error: longhorn deletion timeout"; exit 1 ; fi
+  done
+}
+
+
 install_longhorn(){
   LONGHORN_MANIFEST_FILE_PATH="${1}"
 
@@ -197,11 +212,37 @@ main(){
   adjust_test_size
 
   install_local_path_provisioner
+  run_fio_local_path_test
+
+  if [[ -n "${LONGHORN_PREVIOUS_VERSION}" ]]; then
+    wget "https://raw.githubusercontent.com/longhorn/longhorn/${LONGHORN_PREVIOUS_VERSION}/deploy/longhorn.yaml" -P "${TF_VAR_tf_workspace}"
+    kubectl apply -f "${TF_VAR_tf_workspace}/longhorn.yaml"
+    wait_longhorn_status_running
+
+    adjust_replica_count 3
+    run_fio_longhorn_test 3
+
+    adjust_replica_count 2
+    kubectl cordon "$(kubectl get nodes | awk 'NR!=1 && $3!~/control-plane/ {print $1}' | sort | awk 'NR==3')"
+    run_fio_longhorn_test 2
+
+    adjust_replica_count 1
+    kubectl cordon "$(kubectl get nodes | awk 'NR!=1 && $3!~/control-plane/ {print $1}' | sort | awk 'NR==2')"
+    run_fio_longhorn_test 1
+
+    kubectl uncordon "$(kubectl get nodes | awk 'NR!=1 && $3!~/control-plane/ {print $1}' | sort | awk 'NR==3')"
+    kubectl uncordon "$(kubectl get nodes | awk 'NR!=1 && $3!~/control-plane/ {print $1}' | sort | awk 'NR==2')"
+    kubectl uncordon "$(kubectl get nodes | awk 'NR!=1 && $3!~/control-plane/ {print $1}' | sort | awk 'NR==1')"
+
+    kubectl create -f "https://raw.githubusercontent.com/longhorn/longhorn/${LONGHORN_PREVIOUS_VERSION}/uninstall/uninstall.yaml"
+    kubectl wait --for=condition=complete --timeout=5m job/longhorn-uninstall
+    kubectl delete -f "https://raw.githubusercontent.com/longhorn/longhorn/${LONGHORN_PREVIOUS_VERSION}/deploy/longhorn.yaml" || true
+    kubectl delete -f "https://raw.githubusercontent.com/longhorn/longhorn/${LONGHORN_PREVIOUS_VERSION}/uninstall/uninstall.yaml" || true
+    wait_longhorn_resources_deleted
+  fi
 
   generate_longhorn_yaml_manifest "${TF_VAR_tf_workspace}"
   install_longhorn "${TF_VAR_tf_workspace}/longhorn.yaml"
-
-  run_fio_local_path_test
 
   adjust_replica_count 3
   run_fio_longhorn_test 3
