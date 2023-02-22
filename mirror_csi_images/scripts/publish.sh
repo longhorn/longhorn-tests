b22ed687d8c1b8d38638bf56c34cf34e611bd7f8
Rayan Das <rayandas91@gmail.com>
2023-02-22 12:03:10 +0530

    update k8s.gcr.io to registry.k8s.io
    Signed-off-by: Rayan Das <rayandas91@gmail.com>

--- mirror_csi_images/scripts/publish.sh
+++ mirror_csi_images/scripts/publish.sh
@@ -12,14 +12,14 @@ if [[ -n "${LONGHORN_IMAGES_FILE_URL}" ]]; then
     if [[ "${LINE}" =~ "csi-" ]]; then
       CSI_IMAGE=$(echo "${LINE}" | sed -e "s/longhornio\///g")
       IFS=: read -ra IMAGE_TAG_PAIR <<< "${CSI_IMAGE}"
-      echo "k8s.gcr.io/sig-storage/${IMAGE_TAG_PAIR[0]}" "longhornio/${IMAGE_TAG_PAIR[0]}" "${IMAGE_TAG_PAIR[1]}" >> "${INFILE}"
+      echo "registry.k8s.io/sig-storage/${IMAGE_TAG_PAIR[0]}" "longhornio/${IMAGE_TAG_PAIR[0]}" "${IMAGE_TAG_PAIR[1]}" >> "${INFILE}"
     fi
   done < "${LONGHORN_IMAGES_FILE}"
 else
   IFS=, read -ra CSI_IMAGES_ARR <<< "${CSI_IMAGES}"
   for CSI_IMAGE in "${CSI_IMAGES_ARR[@]}"; do
     IFS=: read -ra IMAGE_TAG_PAIR <<< "$CSI_IMAGE"
-    echo "k8s.gcr.io/sig-storage/${IMAGE_TAG_PAIR[0]}" "longhornio/${IMAGE_TAG_PAIR[0]}" "${IMAGE_TAG_PAIR[1]}" >> "${INFILE}"
+    echo "registry.k8s.io/sig-storage/${IMAGE_TAG_PAIR[0]}" "longhornio/${IMAGE_TAG_PAIR[0]}" "${IMAGE_TAG_PAIR[1]}" >> "${INFILE}"
   done
 fi
 
