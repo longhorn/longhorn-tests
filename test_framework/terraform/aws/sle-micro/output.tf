d21bf5f84ca62377ede0e37d885f83dbb60feff5
Yang Chiu <yang.chiu@suse.com>
2023-03-29 20:22:17 +0800

    ci: add sle-micro provisioning
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

---
+++ test_framework/terraform/aws/sle-micro/output.tf
@@ -0,0 +1,47 @@
+# Generate RKE config file (for rke only)
+output "rke_config" {
+  depends_on = [
+    aws_instance.lh_aws_instance_controlplane_rke,
+    aws_instance.lh_aws_instance_worker_rke,
+    aws_eip.lh_aws_eip_controlplane,
+    aws_eip_association.lh_aws_eip_assoc_rke,
+    null_resource.wait_for_docker_start_controlplane_rke,
+    null_resource.wait_for_docker_start_worker_rke
+  ]
+
+  value = var.k8s_distro_name == "rke" ? yamlencode({
+    "kubernetes_version": var.k8s_distro_version,
+    "nodes": concat(
+     [
+      for idx, controlplane_instance in aws_instance.lh_aws_instance_controlplane_rke : {
+           "address": aws_eip.lh_aws_eip_controlplane[idx].public_ip,
+           "hostname_override": controlplane_instance.tags.Name,
+           "user": "suse",
+           "role": ["controlplane","etcd"]
+          }
+     ],
+     [
+      for worker_instance in aws_instance.lh_aws_instance_worker_rke : {
+           "address": worker_instance.private_ip,
+           "hostname_override": worker_instance.tags.Name,
+           "user": "suse",
+           "role": ["worker"]
+         }
+     ]
+    ),
+    "bastion_host": {
+      "address": aws_eip.lh_aws_eip_controlplane[0].public_ip
+      "user": "suse"
+      "port":  22
+      "ssh_key_path": var.aws_ssh_private_key_file_path
+    }
+  }) : null
+}
+
+output "load_balancer_url" {
+  depends_on = [
+    aws_lb.lh_aws_lb
+  ]
+
+  value = var.create_load_balancer ? aws_lb.lh_aws_lb[0].dns_name : null
+}
---
+++ test_framework/terraform/aws/sle-micro/variables.tf
@@ -0,0 +1,124 @@
+variable "lh_aws_access_key" {
+  type        = string
+  description = "AWS ACCESS_KEY"
+}
+
+variable "lh_aws_secret_key" {
+  type        = string
+  description = "AWS SECRET_KEY"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_availability_zone" {
+  type        = string
+  default     = "us-east-1d"
+}
+
+variable "lh_aws_vpc_name" {
+  type        = string
+  default     = "vpc-lh-tests"
+}
+
+variable "arch" {
+  type        = string
+  description = "available values (amd64, arm64)"
+  default     = "amd64"
+}
+
+variable "os_distro_version" {
+  type        = string
+  default     = "5.3"
+}
+
+variable "aws_ami_sles_account_number" {
+  type        = string
+  default     = "amazon"
+}
+
+variable "lh_aws_instance_count_controlplane" {
+  type        = number
+  default     = 1
+}
+
+variable "lh_aws_instance_count_worker" {
+  type        = number
+  default     = 3
+}
+
+variable "lh_aws_instance_name_controlplane" {
+  type        = string
+  default     = "lh-tests-controlplane"
+}
+
+variable "lh_aws_instance_type_controlplane" {
+  type        = string
+  description = "Recommended instance types t2.xlarge for amd64 & a1.xlarge  for arm64"
+  default     = "t2.xlarge"
+}
+
+variable "lh_aws_instance_type_worker" {
+  type        = string
+  description = "Recommended instance types t2.xlarge for amd64 & a1.xlarge  for arm64"
+  default     = "t2.xlarge"
+}
+
+variable "lh_aws_instance_root_block_device_size_controlplane" {
+  type        = number
+  default     = 40
+}
+
+variable "aws_ssh_public_key_file_path" {
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "aws_ssh_private_key_file_path" {
+  type        = string
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "lh_aws_instance_name_worker" {
+  type        = string
+  default     = "lh-tests-worker"
+}
+
+variable "lh_aws_instance_root_block_device_size_worker" {
+  type        = number
+  default     = 40
+}
+
+variable "k8s_distro_name" {
+  type        = string
+  default     = "k3s"
+  description = "kubernetes distro version to install [rke, k3s, rke2]  (default: k3s)"
+}
+
+variable "k8s_distro_version" {
+  type        = string
+  default     = "v1.25.3+k3s1"
+  description = <<-EOT
+    kubernetes version that will be deployed
+    rke: (default: v1.22.5-rancher1-1)
+    k3s: (default: v1.25.3+k3s1)
+    rke2: (default: v1.25.3+rke2r1)
+  EOT
+}
+
+variable "use_hdd" {
+  type    = bool
+  default = false
+}
+
+variable "create_load_balancer" {
+  type    = bool
+  default = false
+}
+
+variable "registration_code" {
+  type    = string
+  sensitive   = true
+}
