51dc4246719ddb007fffafab502249f8904bb5eb
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-05-15 08:18:36 +0800

    test: remove guaranteed-engine-cpu setting
    ref: 5917
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- manager/integration/tests/common.py
+++ manager/integration/tests/common.py
@@ -167,7 +167,6 @@ SETTING_DEGRADED_AVAILABILITY = \
     "allow-volume-creation-with-degraded-availability"
 SETTING_DISABLE_SCHEDULING_ON_CORDONED_NODE = \
     "disable-scheduling-on-cordoned-node"
-SETTING_GUARANTEED_ENGINE_CPU = "guaranteed-engine-cpu"
 SETTING_GUARANTEED_ENGINE_MANAGER_CPU = "guaranteed-engine-manager-cpu"
 SETTING_GUARANTEED_REPLICA_MANAGER_CPU = "guaranteed-replica-manager-cpu"
 SETTING_GUARANTEED_INSTANCE_MANAGER_CPU = "guaranteed-instance-manager-cpu"
--- manager/integration/tests/test_settings.py
+++ manager/integration/tests/test_settings.py
@@ -27,7 +27,6 @@ from common import (  # NOQA
 
     LONGHORN_NAMESPACE,
     SETTING_TAINT_TOLERATION,
-    SETTING_GUARANTEED_ENGINE_CPU,
     SETTING_GUARANTEED_INSTANCE_MANAGER_CPU,
     SETTING_PRIORITY_CLASS,
     SETTING_DEFAULT_REPLICA_COUNT,
@@ -365,35 +364,30 @@ def test_instance_manager_cpu_reservation(client, core_api):  # NOQA
     Test if the CPU requests of instance manager pods are controlled by
     the settings and the node specs correctly.
 
-    1. Try to change the deprecated setting `Guaranteed Engine CPU`.
-       --> The setting update should fail.
-    2. On node 1, set `node.instanceManagerCPURequest` to 150.
+    1. On node 1, set `node.instanceManagerCPURequest` to 150.
        --> The IM pods on this node will be restarted. And the CPU requests
        of these IM pods matches the above milli value.
-    3. Change the new setting `Guaranteed Instance Manager CPU` to 10,
+    2. Change the new setting `Guaranteed Instance Manager CPU` to 10,
        Then wait for all IM pods except for the pods on node 1 restarting.
        --> The CPU requests of the restarted IM pods equals to
            the new setting value multiply the kube node allocatable CPU.
-    4. Set the new settings to 0.
+    3. Set the new settings to 0.
        --> All IM pods except for the pod on node 1 will be restarted without
         CPU requests.
-    5. Set the fields on node 1 to 0.
+    4. Set the fields on node 1 to 0.
        --> The IM pods on node 1 will be restarted without CPU requests.
-    6. Set the new setting to a values smaller than 40.
+    5. Set the new setting to a values smaller than 40.
        Then wait for all IM pods restarting.
        --> The CPU requests of all IM pods equals to
            the new setting value multiply the kube node allocatable CPU.
-    7. Set the new setting to a value greater than 40.
+    6. Set the new setting to a value greater than 40.
        --> The setting update should fail.
-    8. Create a volume, verify everything works as normal
+    7. Create a volume, verify everything works as normal
 
     Note: use fixture to restore the setting into the original state
     """
 
     instance_managers = client.list_instance_manager()
-    deprecated_setting = client.by_id_setting(SETTING_GUARANTEED_ENGINE_CPU)
-    with pytest.raises(Exception) as e:
-        client.update(deprecated_setting, value="0.1")
 
     host_node_name = get_self_host_id()
     host_node = client.by_id_node(host_node_name)
