d09472b4847768e77c363e37506ddc50930d2f22
Yang Chiu <yang.chiu@suse.com>
2023-06-13 08:55:16 +0800

    ci: wait for csi components created when installing longhorn
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>
    (cherry picked from commit 6b21632eec2724c07086c4d1ba366f8719d67324)

--- test_framework/scripts/longhorn-setup.sh
+++ test_framework/scripts/longhorn-setup.sh
@@ -51,12 +51,14 @@ wait_longhorn_status_running(){
     local RETRY_COUNTS=10  # in minutes
 	local RETRY_INTERVAL="1m"
 
-    RETRIES=0
-    while [[ -n `kubectl get pods -n ${LONGHORN_NAMESPACE} --no-headers | awk '{print $3}' | grep -v Running` ]]; do
-        echo "Longhorn is still installing ... re-checking in 1m"
-        sleep ${RETRY_INTERVAL}
-        RETRIES=$((RETRIES+1))
-
+  # csi components are installed after longhorn components.
+  # it's possible that all longhorn components are running but csi components aren't created yet.
+  RETRIES=0
+  while [[ -z `kubectl get pods -n ${LONGHORN_NAMESPACE} --no-headers 2>&1 | awk '{print $1}' | grep csi-` ]] || \
+    [[ -n `kubectl get pods -n ${LONGHORN_NAMESPACE} --no-headers 2>&1 | awk '{print $3}' | grep -v Running` ]]; do
+    echo "Longhorn is still installing ... re-checking in 1m"
+    sleep ${RETRY_INTERVAL}
+    RETRIES=$((RETRIES+1))
         if [[ ${RETRIES} -eq ${RETRY_COUNTS} ]]; then echo "Error: longhorn installation timeout"; exit 1 ; fi
     done
 }
