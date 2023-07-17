c163a53655a64ad3556993af1102d49b679853b4
Ray Chang <ray.chang@suse.com>
2023-07-17 21:49:38 +0800

    test(negative): add node not ready library: volume related
    Longhorn 5556
    
    Signed-off-by: Ray Chang <ray.chang@suse.com>

--- e2e/libs/volume/crd.py
+++ e2e/libs/volume/crd.py
@@ -1,10 +1,11 @@
-import os
 import time
-import warnings
+import logging
 
+from utils.common_utils import k8s_cr_api
 from volume.base import Base
 from volume.rest import Rest
 from kubernetes import client
+from kubernetes.client.rest import ApiException
 
 Ki = 2**10
 Mi = 2**20
@@ -17,11 +18,44 @@ retry_interval = 1
 class CRD(Base):
 
     def __init__(self, node_exec):
-        self.obj_api = client.CustomObjectsApi()
         self.node_exec = node_exec
 
-    def get(self, volume_name):
-        volume = self.obj_api.get_namespaced_custom_object(
+    def create(self, volume_name, size, replica_count, volume_type):
+        logging.info(
+            f"creating {size} Gi {volume_type} volume with {replica_count} replicas")
+
+        volume_body = {
+            "apiVersion": "longhorn.io/v1beta2",
+            "kind": "Volume",
+            "metadata": {"name": volume_name},
+            "spec": {
+                "frontend": "blockdev",
+                "replicaAutoBalance": "ignored",
+                "size": str(int(size) * Gi),
+                "numberOfReplicas": int(replica_count),
+                "accessMode": volume_type.lower()
+            }
+        }
+
+        k8s_cr_api().create_namespaced_custom_object(
+            group="longhorn.io",
+            version="v1beta2",
+            namespace="longhorn-system",
+            plural="volumes",
+            body=volume_body
+        )
+
+    def create_with_manifest(self, manifest):
+        k8s_cr_api().create_namespaced_custom_object(
+            group="longhorn.io",
+            version="v1beta2",
+            namespace="longhorn-system",
+            plural="volumes",
+            body=manifest
+        )
+
+    def get(self, volume_name=""):
+        volume = k8s_cr_api().get_namespaced_custom_object(
             group="longhorn.io",
             version="v1beta2",
             namespace="longhorn-system",
@@ -30,91 +64,107 @@ class CRD(Base):
         )
         return volume
 
-    def create(self, volume_name, size, replica_count):
-        body = {
-            "apiVersion": "longhorn.io/v1beta2",
-            "kind": "Volume",
-            "metadata": {"name": volume_name},
-            "spec": {
-                "frontend": "blockdev",
-                "replicaAutoBalance": "ignored",
-                "size": str(int(size) * Gi),
-                "numberOfReplicas": int(replica_count)
-            }
-        }
-        self.obj_api.create_namespaced_custom_object(
-            group="longhorn.io",
-            version="v1beta2",
-            namespace="longhorn-system",
-            plural="volumes",
-            body=body
-        )
+    def delete(self, volume_name):
+        if volume_name == "":
+            logging.info("deleting all volumes")
+        else:
+            logging.info(f"deleting the volume {volume_name}")
+
+        resp = self.get(volume_name)
+        assert resp != "", "failed to get the volume"
+
+        volume_list = resp['items']
+        if len(volume_list) == 0:
+            logging.warning("cannot find the volume")
+            return
+
+        for volume in volume_list:
+            volume_name = volume['metadata']['name']
+            k8s_cr_api().delete_namespaced_custom_object(
+                group="longhorn.io",
+                version="v1beta2",
+                namespace="longhorn-system",
+                plural="volumes",
+                name=volume_name
+            )
+        logging.info("finished deleting volume")
 
     def attach(self, volume_name, node_name):
-        body = {
-            "spec": {
-                "nodeID": node_name
-            }
-        }
-        self.obj_api.patch_namespaced_custom_object(
+        k8s_cr_api().patch_namespaced_custom_object(
             group="longhorn.io",
             version="v1beta2",
             namespace="longhorn-system",
             plural="volumes",
             name=volume_name,
-            body=body
+            body={
+                    "spec": {
+                        "nodeID": node_name
+                    }
+            }
         )
+
+        try:
+            k8s_cr_api().patch_namespaced_custom_object(
+                group="longhorn.io",
+                version="v1beta2",
+                namespace="longhorn-system",
+                plural="volumeattachments",
+                name=volume_name,
+                body={
+                        "spec": {
+                            "attachmentTickets": {
+                                "": {
+                                    "generation": 0,
+                                    "id": "",
+                                    "nodeID": node_name,
+                                    "parameters": {
+                                        "disableFrontend": "false",
+                                        "lastAttachedBy": ""
+                                    },
+                                    "type": "longhorn-api"
+                                }
+                            }
+                        }
+                }
+            )
+        except ApiException as e:
+            # new CRD: volumeattachments was added since from 1.5.0
+            # https://github.com/longhorn/longhorn/issues/3715
+            if e.reason != "Not Found":
+                Exception(f'exception for creating volumeattachments:', e)
+
         self.wait_for_volume_state(volume_name, "attached")
 
     def wait_for_volume_state(self, volume_name, desired_state):
-        print(self.get(volume_name)["status"]["state"])
+        logging.info(
+            f"waiting volume {volume_name} state becomes {desired_state}")
         for i in range(retry_count):
-            if self.get(volume_name)["status"]["state"] == desired_state:
+            volume_state = self.get(volume_name)["status"]["state"]
+            if volume_state == desired_state:
                 break
             time.sleep(retry_interval)
         assert self.get(volume_name)["status"]["state"] == desired_state
 
+    def get_volume_state(self, volume_name):
+        logging.info(f"getting volume {volume_name} state")
+        return self.get(volume_name)["status"]["robustness"]
+
     def get_endpoint(self, volume_name):
-        warnings.warn("no endpoint in volume cr, get it from rest api")
+        logging.info(f"getting volume {volume_name} endpoint")
         return Rest(self.node_exec).get_endpoint(volume_name)
 
     def write_random_data(self, volume_name, size):
+        logging.info(
+            f"writing {size} Mb data into volume {volume_name} mount point")
+
         node_name = self.get(volume_name)["spec"]["nodeID"]
         endpoint = self.get_endpoint(volume_name)
+
         checksum = self.node_exec.issue_cmd(
             node_name,
-            f"dd if=/dev/urandom of={endpoint} bs=1M count={size} status=none;\
+            f"dd if=/dev/urandom of={endpoint} bs=2M count={size} status=none;\
               md5sum {endpoint} | awk \'{{print $1}}\'")
         return checksum
-
-    def delete_replica(self, volume_name, node_name):
-        replica_list = self.obj_api.list_namespaced_custom_object(
-            group="longhorn.io",
-            version="v1beta2",
-            namespace="longhorn-system",
-            plural="replicas",
-            label_selector=f"longhornvolume={volume_name}\
-                             ,longhornnode={node_name}"
-        )
-        print(f"delete replica {replica_list['items'][0]['metadata']['name']}")
-        self.obj_api.delete_namespaced_custom_object(
-            group="longhorn.io",
-            version="v1beta2",
-            namespace="longhorn-system",
-            plural="replicas",
-            name=replica_list['items'][0]['metadata']['name']
-        )
-
-    def wait_for_replica_rebuilding_start(self, volume_name, node_name):
-        warnings.warn("no rebuild status in volume cr, get it from rest api")
-        Rest(self.node_exec).wait_for_replica_rebuilding_start(volume_name, node_name)
-
-    def wait_for_replica_rebuilding_complete(self, volume_name, node_name):
-        warnings.warn("no rebuild status in volume cr, get it from rest api")
-        Rest(self.node_exec).wait_for_replica_rebuilding_complete(
-            volume_name,
-            node_name
-        )
 
     def check_data(self, volume_name, checksum):
         node_name = self.get(volume_name)["spec"]["nodeID"]
@@ -122,6 +172,5 @@ class CRD(Base):
         _checksum = self.node_exec.issue_cmd(
             node_name,
             f"md5sum {endpoint} | awk \'{{print $1}}\'")
-        print(f"get {endpoint} checksum = {_checksum},\
-                expected checksum = {checksum}")
-        assert _checksum == checksum
+        if _checksum != checksum:
+            Exception(f"data was changed: {_checksum}/{checksum}")
