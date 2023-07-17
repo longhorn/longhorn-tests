82652253303890209889d0dfebf524ce51a40de4
Ray Chang <ray.chang@suse.com>
2023-07-17 21:49:38 +0800

    test(negative): add node not ready library: volume related
    Longhorn 5556
    
    Signed-off-by: Ray Chang <ray.chang@suse.com>

--- e2e/libs/volume/volume.py
+++ e2e/libs/volume/volume.py
@@ -1,3 +1,5 @@
+import logging
+
 from volume.base import Base
 from volume.crd import CRD
 from volume.rest import Rest
@@ -12,13 +14,19 @@ class Volume(Base):
         if self._strategy == LonghornOperationStrategy.CRD:
             self.volume = CRD(node_exec)
         else:
-            self.volume = REST(node_exec)
+            self.volume = Rest(node_exec)
+
+    def create(self, volume_name, size, replica_count, volume_type):
+        return self.volume.create(volume_name, size, replica_count, volume_type)
+
+    def create_with_manifest(self, manifest):
+        return self.volume.create_with_manifest(manifest)
 
     def get(self, volume_name):
         return self.volume.get(volume_name)
 
-    def create(self, volume_name, size, replica_count):
-        return self.volume.create(volume_name, size, replica_count)
+    def delete(self, volume_name=""):
+        return self.volume.delete(volume_name)
 
     def attach(self, volume_name, node_name):
         return self.volume.attach(volume_name, node_name)
@@ -26,26 +34,14 @@ class Volume(Base):
     def wait_for_volume_state(self, volume_name, desired_state):
         return self.volume.wait_for_volume_state(volume_name, desired_state)
 
+    def get_volume_state(self, volume_name):
+        return self.volume.get_volume_state(volume_name)
+
     def get_endpoint(self, volume_name):
         return self.volume.get_endpoint(volume_name)
 
     def write_random_data(self, volume_name, size):
         return self.volume.write_random_data(volume_name, size)
-
-    def delete_replica(self, volume_name, node_name):
-        return self.volume.delete_replica(volume_name, node_name)
-
-    def wait_for_replica_rebuilding_start(self, volume_name, node_name):
-        return self.volume.wait_for_replica_rebuilding_start(
-            volume_name,
-            node_name
-        )
-
-    def wait_for_replica_rebuilding_complete(self, volume_name, node_name):
-        return self.volume.wait_for_replica_rebuilding_complete(
-            volume_name,
-            node_name
-        )
 
     def check_data(self, volume_name, checksum):
         return self.volume.check_data(volume_name, checksum)
