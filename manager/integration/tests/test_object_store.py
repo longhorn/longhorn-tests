42b1e26e4ba97be6f94aa7f37eb263ad0e00c781
Moritz Röhrich <moritz.rohrich@suse.com>
2023-09-06 12:01:25 +0200

    object store: rename test suite, adjust CRD
    - Adjust to ObjectStore CRD
    - Rename test suite for object store controller
    
    Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

--- manager/integration/tests/test_object_store.py
+++ manager/integration/tests/test_object_store.py
@@ -1,10 +1,10 @@
 """
-Test Object Endpoint
+Test Object Store Controller
 ---
 
-This module excercises the object endpoint controller, which is part of the
-longhorn manager and controls the deployment of S3 endpoints according to
-ObjectEndpoint CRDs.
+This module excercises the object store controller, which is part of the
+longhorn manager and controls the deployment of S3 stores and their endpoints
+according to ObjectStore CRDs.
 """
 import random
 import string
@@ -19,106 +19,76 @@ from kubernetes.client.rest import ApiException
 from common import get_core_api_client
 from common import get_apps_api_client
 from common import get_custom_object_api_client
-from common import create_storage_class, delete_storage_class
 from common import LONGHORN_NAMESPACE
 
 LONGHORN_API_GROUP = "longhorn.io"
 LONGHORN_API_VERSION = "v1beta2"
+LONGHORN_API_KIND = "ObjectStore"
+LONGHORN_API_KIND_PLURAL = "objectstores"
 
-OBJECT_ENDPOINT_LABEL = "Object Endpoint"
-OBJECT_ENDPOINT_NAME = "object-endpoint"
-OBJECT_ENDPOINT_SIZE = "2Gi"
-OBJECT_ENDPOINT_STORAGE_CLASS_NAME = "sc-test-object-endpoint"
+OBJECT_STORE_LABEL = "Object Store"
+OBJECT_STORE_NAME = "object-store"
+OBJECT_STORE_SIZE = "2Gi"
 
-DEFAULT_OBJECT_ENDPOINT_NAME = "test-object-endpoint"
-DEFAULT_OBJECT_ENDPOINT_ACCESS_KEY = "foobar"
-DEFAULT_OBJECT_ENDPOINT_SECRET_KEY = "barfoo"
+DEFAULT_OBJECT_STORE_NAME = "test-object-store"
+DEFAULT_OBJECT_STORE_ACCESS_KEY = "foobar"
+DEFAULT_OBJECT_STORE_SECRET_KEY = "barfoo"
 
 WAIT_INTERVAL = 1  # one second
 WAIT_TIMEOUT = 120  # two minutes
 
 
 @pytest.fixture
-def storage_class():
+def object_store():  # pylint: disable=W0621
     """
-    Storage Class pytest fixture
-    This fixture yields a storage class in the cluster and the manifest to the
-    callers location. When the calling test is finished (with or without
-    failure) the storage class is removed from the cluster
-    The fixture has a partly randomly generated name to avoid tests influencing
-    each other due to name clashes.
-    """
-    uid = gen_id(6)
-    manifest = {
-        'apiVersion': 'storage.k8s.io/v1',
-        'kind': 'StorageClass',
-        'metadata': {
-            'name': f"{OBJECT_ENDPOINT_STORAGE_CLASS_NAME}-{uid}",
-        },
-        'provisioner': 'driver.longhorn.io',
-        'allowVolumeExpansion': True,
-        'reclaimPolicy': 'Delete',
-        'parameters': {
-            'numberOfReplicas': '3',
-            'staleReplicaTimeout': '30',
-        },
-    }
-    create_storage_class(manifest)
-    yield manifest
-    delete_storage_class(manifest['metadata']['name'])
-
-
-@pytest.fixture
-def object_endpoint(storage_class):  # pylint: disable=W0621
-    """
-    Object Endpoint pytest fixture
-    This fixture yields an object endpoint in the cluster and the manifest to
+    Object Store pytest fixture
+    This fixture yields an object store in the cluster and the manifest to
     the callers location. When the calling test is finished (with or without
-    failure) the object endpoint is removed from the cluster.
-    It is not an error if the object endpoint is removed explicitly during the
+    failure) the object store is removed from the cluster.
+    It is not an error if the object store is removed explicitly during the
     calling test.
     The fixture has a partly randomly generated name to avoid tests influencing
     each other due to name clashes.
     """
     uid = gen_id(6)
     manifest = {
-        'apiVersion': 'longhorn.io/v1beta2',
-        'kind': 'ObjectEndpoint',
+        'apiVersion': f"{LONGHORN_API_GROUP}/{LONGHORN_API_VERSION}",
+        'kind': LONGHORN_API_KIND,
         'metadata': {
-            'name': f"{DEFAULT_OBJECT_ENDPOINT_NAME}-{uid}",
+            'name': f"{DEFAULT_OBJECT_STORE_NAME}-{uid}",
         },
         'spec': {
-            'storageClassName': storage_class['metadata']['name'],
-            'size': OBJECT_ENDPOINT_SIZE,
+            'storage': {
+                'size': OBJECT_STORE_SIZE,
+            },
             'credentials': {
-                'accessKey': DEFAULT_OBJECT_ENDPOINT_ACCESS_KEY,
-                'secretKey': DEFAULT_OBJECT_ENDPOINT_SECRET_KEY,
+                'accessKey': DEFAULT_OBJECT_STORE_ACCESS_KEY,
+                'secretKey': DEFAULT_OBJECT_STORE_SECRET_KEY,
             },
         }
     }
-    create_object_endpoint(manifest)
+    create_object_store(manifest)
     yield manifest
-    delete_object_endpoint(manifest)
-    wait_object_endpoint_removed(manifest)
+    delete_object_store(manifest)
+    wait_object_store_removed(manifest)
 
 
-@pytest.mark.object_endpoint
-def test_create_object_endpoint(object_endpoint):  # pylint: disable=W0621
+@pytest.mark.object_store
+def test_create_object_store(object_store):  # pylint: disable=W0621
     """
-    Scenario: test the creation of an object storage endpoint
+    Scenario: test the creation of an object store
 
     Given:
-    - StorageClass
     - Size
     - Name
     - Access Key, Secret Key
 
-    When an ObjectEndpoint resource is created:
+    When an ObjectStore resource is created:
     - Create a Deployment, Secret, Service, PVC, PV and Longhorn volume
 
-    Wait for the ObjectEndpoint to transition into "Running" state
+    Wait for the ObjectStore to transition into "Running" state
 
-    When the ObjectEndpoint is in "Runnin" state:
+    When the ObjectStore is in "Runnin" state:
     - The Longhorn volume must be in "attached" state
     - The PV must be bound
     - The PVC must be bound
@@ -126,35 +96,35 @@ def test_create_object_endpoint(object_endpoint):  # pylint: disable=W0621
     - The service must have a port
     - The deployment must have exactly one expected, desired, actual and
       available replica
-    - The object endpoint must be in "Running" state
-    - The object endpoint must have and addres containing its own name in
-      its `.status.endpoint` property
-    - The address from the object endpoints `.status.endpoint` must answer to
+    - The object store must be in "Running" state
+    - The object store must have and addres containing its own name in
+      its `.status.endpoints` property
+    - The address from the object store's `.status.endpoints` must answer to
       S3 requests.
     """
-    wait_object_endpoint_running(object_endpoint)
+    wait_object_store_running(object_store)
 
-    assert_longhorn_volume_ready(object_endpoint)
-    assert_persistent_volume_ready(object_endpoint)
-    assert_persistent_volume_claim_ready(object_endpoint)
-    assert_secret_ready(object_endpoint)
-    assert_service_ready(object_endpoint)
-    assert_deployment_ready(object_endpoint)
-    assert_object_endpoint_running(object_endpoint)
-    assert_object_endpoint_has_endpoint(object_endpoint)
-    assert_object_endpoint_smoke_s3(object_endpoint)
+    assert_longhorn_volume_ready(object_store)
+    assert_persistent_volume_ready(object_store)
+    assert_persistent_volume_claim_ready(object_store)
+    assert_secret_ready(object_store)
+    assert_service_ready(object_store)
+    assert_deployment_ready(object_store)
+    assert_object_store_running(object_store)
+    assert_object_store_has_endpoints(object_store)
+    assert_object_store_smoke_s3(object_store)
 
 
-@pytest.mark.object_endpoint
-def test_delete_object_endpoint(object_endpoint):  # pylint: disable=W0621
+@pytest.mark.object_store
+def test_delete_object_store(object_store):  # pylint: disable=W0621
     """
-    Scenario: test the deletion of an object storage endpoint
+    Scenario: test the deletion of an object store
 
     Given:
-    - An ObjectEndpoint in "Running" state and its associated resources
+    - An ObjectStore in "Running" state and its associated resources
       (Deployment, PVC, Secret, Service, PV and Longhorn volume)
 
-    When the object endpoint is deleted:
+    When the object store is deleted:
     - The Deployment is removed
     - The Service is removed
     - The Secret is removed
@@ -162,48 +132,60 @@ def test_delete_object_endpoint(object_endpoint):  # pylint: disable=W0621
     - The PersistenVolume is removed
     - The Longhorn volume is removed
     """
-    wait_object_endpoint_running(object_endpoint)
-    delete_object_endpoint(object_endpoint)
-    wait_object_endpoint_removed(object_endpoint)
+    wait_object_store_running(object_store)
+    delete_object_store(object_store)
+    wait_object_store_removed(object_store)
 
-    assert_deployment_removed(object_endpoint)
-    assert_service_removed(object_endpoint)
-    assert_secret_removed(object_endpoint)
-    assert_persistent_volume_claim_removed(object_endpoint)
-    assert_persistent_volume_removed(object_endpoint)
-    assert_longhorn_volume_removed(object_endpoint)
+    assert_deployment_removed(object_store)
+    assert_service_removed(object_store)
+    assert_secret_removed(object_store)
+    assert_persistent_volume_claim_removed(object_store)
+    assert_persistent_volume_removed(object_store)
+    assert_longhorn_volume_removed(object_store)
 
 # - - -
-# Below here are utilities and helper functions for testing the object endpoint
+# Below here are utilities and helper functions for testing the object store
 # controller.
 #
 # pylint: disable=missing-function-docstring
 
 
-def assert_object_endpoint_running(manifest):
-    endpoint = get_object_endpoint(manifest)
-    assert endpoint is not None
-    assert "status" in endpoint
-    assert endpoint['status']['state'] == "Running"
+def assert_object_store_running(manifest):
+    store = get_object_store(manifest)
+    assert store is not None
+    assert "status" in store
+    assert store['status']['state'] == "Running"
 
 
-def assert_object_endpoint_has_endpoint(manifest):
-    endpoint = get_object_endpoint(manifest)
-    assert endpoint is not None
-    assert "status" in endpoint
-    assert endpoint['status']['endpoint'] is not None
-    assert endpoint['status']['endpoint'] != ""
-    assert endpoint['metadata']['name'] in endpoint['status']['endpoint']
+def assert_object_store_has_endpoints(manifest):
+
+    def name_is_part_of_an_endpoint(name, endpoints):
+        """
+        Takes a name and a list of endpoint urls and returns true if the name
+        occurs in any of the endpoint urls.
+        """
+        for url in endpoints:
+            if name in url:
+                return True
+        return False
+
+    store = get_object_store(manifest)
+    assert store is not None
+    assert "status" in store
+    assert store['status']['endpoints'] is not None
+    assert len(store['status']['endpoints']) > 0
+    assert name_is_part_of_an_endpoint(store['metadata']['name'],
+                                       store['status']['endpoints'])
 
 
-def assert_object_endpoint_smoke_s3(manifest):
-    endpoint = get_object_endpoint(manifest)
+def assert_object_store_smoke_s3(manifest):
+    store = get_object_store(manifest)
 
     client = boto3.session.Session().client(
         service_name='s3',
-        aws_access_key_id=endpoint['spec']['credentials']['accessKey'],
-        aws_secret_access_key=endpoint['spec']['credentials']['secretKey'],
-        endpoint_url=f"http://{endpoint['status']['endpoint']}",
+        aws_access_key_id=store['spec']['credentials']['accessKey'],
+        aws_secret_access_key=store['spec']['credentials']['secretKey'],
+        endpoint_url=f"http://{store['status']['endpoints'][0]}",
         use_ssl=False,
     )
 
@@ -224,7 +206,7 @@ def assert_deployment_ready(manifest):
     ready_replicas = deployment.status.ready_replicas
     unavailable_replicas = deployment.status.unavailable_replicas
 
-    assert desired_replicas == 1  # always 1 replica of s3gw per endpoint
+    assert desired_replicas == 1  # always 1 replica of s3gw per object store
     assert actual_replicas == desired_replicas
     assert available_replicas == actual_replicas
     assert ready_replicas == available_replicas
@@ -245,7 +227,7 @@ def assert_deployment_removed(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    err_prefix = f"object endpoint {name}"
+    err_prefix = f"object store {name}"
     assert False, f"{err_prefix} left orphaned deployment {name} after timeout"
 
 
@@ -273,7 +255,7 @@ def assert_service_removed(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    err_prefix = f"object endpoint {name}"
+    err_prefix = f"object store {name}"
     assert False, f"{err_prefix} left orphaned service {name}"
 
 
@@ -303,14 +285,14 @@ def assert_secret_removed(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    err_prefix = f"object endpoint {name}"
+    err_prefix = f"object store {name}"
     assert False, f"{err_prefix} left orphaned secret {name}"
 
 
 def assert_persistent_volume_claim_ready(manifest):
     api = get_core_api_client()
-    endpoint = manifest['metadata']['name']
-    name = f"pvc-{endpoint}"
+    store = manifest['metadata']['name']
+    name = f"pvc-{store}"
 
     try:
         pvc = api.read_namespaced_persistent_volume_claim(name,
@@ -323,8 +305,8 @@ def assert_persistent_volume_claim_ready(manifest):
 
 def assert_persistent_volume_claim_removed(manifest):
     api = get_core_api_client()
-    endpoint = manifest['metadata']['name']
-    name = f"pvc-{endpoint}"
+    store = manifest['metadata']['name']
+    name = f"pvc-{store}"
 
     for _ in range(WAIT_TIMEOUT):
         try:
@@ -336,14 +318,14 @@ def assert_persistent_volume_claim_removed(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    err_prefix = f"object endpoint {name}"
+    err_prefix = f"object store {name}"
     assert False, f"{err_prefix} left orphaned persistent volume claim {name}"
 
 
 def assert_persistent_volume_ready(manifest):
     api = get_core_api_client()
-    endpoint = manifest['metadata']['name']
-    name = f"pv-{endpoint}"
+    store = manifest['metadata']['name']
+    name = f"pv-{store}"
 
     try:
         pv = api.read_persistent_volume(name)  # pylint: disable=invalid-name
@@ -355,8 +337,8 @@ def assert_persistent_volume_ready(manifest):
 
 def assert_persistent_volume_removed(manifest):
     api = get_core_api_client()
-    endpoint = manifest['metadata']['name']
-    name = f"pv-{endpoint}"
+    store = manifest['metadata']['name']
+    name = f"pv-{store}"
 
     for _ in range(WAIT_TIMEOUT):
         try:
@@ -367,7 +349,7 @@ def assert_persistent_volume_removed(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    err_prefix = f"object endpoint {name}"
+    err_prefix = f"object store {name}"
     assert False, f"{err_prefix} left orphaned persistent volume {name}"
 
 
@@ -404,18 +386,18 @@ def assert_longhorn_volume_removed(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    err_prefix = f"object endpoint {name}"
+    err_prefix = f"object store {name}"
     assert False, f"{err_prefix} left orphaned longhorn volume {name}"
 
 
-def wait_object_endpoint_running(manifest):
+def wait_object_store_running(manifest):
     api = get_custom_object_api_client()
     name = manifest['metadata']['name']
 
     for _ in range(WAIT_TIMEOUT):
         status = api.get_cluster_custom_object_status(LONGHORN_API_GROUP,
                                                       LONGHORN_API_VERSION,
-                                                      "objectendpoints",
+                                                      LONGHORN_API_KIND_PLURAL,
                                                       name)
         if status is not None and \
                 "status" in status and \
@@ -424,10 +406,10 @@ def wait_object_endpoint_running(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    assert False, "timed out waiting for object endpoint"
+    assert False, "timed out waiting for object store"
 
 
-def wait_object_endpoint_removed(manifest):
+def wait_object_store_removed(manifest):
     api = get_custom_object_api_client()
     name = manifest['metadata']['name']
 
@@ -435,7 +417,7 @@ def wait_object_endpoint_removed(manifest):
         try:
             api.get_cluster_custom_object(LONGHORN_API_GROUP,
                                           LONGHORN_API_VERSION,
-                                          "objectendpoints",
+                                          LONGHORN_API_KIND_PLURAL,
                                           name)
         except ApiException as exception:
             assert exception.status == 404, "API Error"
@@ -443,35 +425,35 @@ def wait_object_endpoint_removed(manifest):
 
         time.sleep(WAIT_INTERVAL)
 
-    assert False, "timed out waiting for object endpoint to be removed"
+    assert False, "timed out waiting for object store to be removed"
 
 
-def create_object_endpoint(manifest):
+def create_object_store(manifest):
     api = get_custom_object_api_client()
     api.create_cluster_custom_object(LONGHORN_API_GROUP,
                                      LONGHORN_API_VERSION,
-                                     "objectendpoints",
+                                     LONGHORN_API_KIND_PLURAL,
                                      manifest)
 
 
-def delete_object_endpoint(manifest):
+def delete_object_store(manifest):
     api = get_custom_object_api_client()
     try:
         api.delete_cluster_custom_object(LONGHORN_API_GROUP,
                                          LONGHORN_API_VERSION,
-                                         "objectendpoints",
+                                         LONGHORN_API_KIND_PLURAL,
                                          name=manifest['metadata']['name'],
                                          body=k8sclient.V1DeleteOptions())
     except ApiException as exception:
         assert exception.status == 404
 
 
-def get_object_endpoint(manifest):
+def get_object_store(manifest):
     api = get_custom_object_api_client()
     name = manifest['metadata']['name']
     return api.get_cluster_custom_object(LONGHORN_API_GROUP,
                                          LONGHORN_API_VERSION,
-                                         "objectendpoints",
+                                         LONGHORN_API_KIND_PLURAL,
                                          name)
 
 
