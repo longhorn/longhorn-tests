adc173d0b5e7626b9cff3c99bcec6ad9e9197eeb
Yang Chiu <yang.chiu@suse.com>
2023-10-06 13:54:12 +0800

    test: implement kubelet restart
    currently only support k3s
    
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- e2e/libs/workload/pod.py
+++ e2e/libs/workload/pod.py
@@ -2,14 +2,14 @@ import time
 
 from kubernetes import client
 
-from robot.libraries.BuiltIn import BuiltIn
-
 from utility.utility import logging
 from utility.utility import generate_name
+from utility.utility import get_retry_count_and_interval
 
 
 IMAGE_BUSYBOX = 'busybox:1.34.0'
 IMAGE_LITMUX = 'litmuschaos/go-runner:latest'
+IMAGE_UBUNTU = 'ubuntu:16.04'
 
 def new_pod_manifest(image="", command=[], args=[],
                      claim_name="", node_name="", labels={}):
@@ -31,15 +31,35 @@ def new_pod_manifest(image="", command=[], args=[],
         },
         'spec': {
             'nodeName': node_name,
+            'restartPolicy': 'Never',
             'containers': [{
                 'image': image,
                 'imagePullPolicy': 'IfNotPresent',
+                'securityContext': {
+                    'privileged': True
+                },
                 'name': 'run',
                 'command': command,
                 'args': args,
-                'volumeMounts': [],
+                'volumeMounts': [{
+                    'name': 'bus',
+                    'mountPath': '/var/run'
+                }, {
+                    'name': 'rancher',
+                    'mountPath': '/var/lib/rancher'
+                }]
             }],
-            'volumes': []
+            'volumes': [{
+                'name': 'bus',
+                'hostPath': {
+                    'path': '/var/run'
+                }
+            }, {
+                'name': 'rancher',
+                'hostPath': {
+                    'path': '/var/lib/rancher'
+                }
+            }]
         }
     }
 
@@ -73,15 +93,33 @@ def create_pod(manifest, is_wait_for_pod_running=False):
 
 def delete_pod(name, namespace='default'):
     core_api = client.CoreV1Api()
-    core_api.delete_namespaced_pod(name=name, namespace=namespace)
+    try:
+        core_api.delete_namespaced_pod(name=name, namespace=namespace)
+        wait_delete_pod(name)
+    except ApiException as e:
+        assert e.status == 404
+
+def wait_delete_pod(name, namespace='default'):
+    api = client.CoreV1Api()
+    retry_count, retry_interval = get_retry_count_and_interval()
+    for i in range(retry_count):
+        ret = api.list_namespaced_pod(namespace=namespace)
+        found = False
+        for item in ret.items:
+            if item.metadata.name == name:
+                found = True
+                break
+        if not found:
+            break
+        time.sleep(retry_interval)
+    assert not found
 
 def get_pod(name, namespace='default'):
     core_api = client.CoreV1Api()
     return core_api.read_namespaced_pod(name=name, namespace=namespace)
 
-def wait_for_pod_status(name, status, namespace='default',
-                        retry_count=int(BuiltIn().get_variable_value("${RETRY_COUNT}")),
-                        retry_interval=int(BuiltIn().get_variable_value("${RETRY_INTERVAL}"))):
+def wait_for_pod_status(name, status, namespace='default'):
+    retry_count, retry_interval = get_retry_count_and_interval()
     is_running = False
     for i in range(retry_count):
         pod = get_pod(name, namespace)
