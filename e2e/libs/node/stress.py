0f623f6ab998461017457206af0f2c37b8e62001
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-10-24 12:25:20 +0800

    test(negative): stress volume node CPU while replica rebuilding
    ref: 6705
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- e2e/libs/node/stress.py
+++ e2e/libs/node/stress.py
@@ -1,7 +1,18 @@
+from kubernetes import client
+
+from node.utility import get_node_cpu_cores
+
 from utility.utility import logging
 
+from workload.pod import create_pod
 from workload.pod import delete_pod
+from workload.pod import new_pod_manifest
 from workload.workload import get_workload_pods
+
+from workload.pod import IMAGE_LITMUX
+
+NODE_CPU_LOAD_PERCENTAGE = 100
+NODE_STRESS_TIMEOUT_SECOND = 300
 
 LABEL_STRESS_HELPER = "longhorn-stress-helper"
 
@@ -10,3 +21,19 @@ class Stress:
         for pod in get_workload_pods(LABEL_STRESS_HELPER):
             logging(f"Cleaning up stress pod {pod.metadata.name}")
             delete_pod(pod.metadata.name, pod.metadata.namespace)
+
+    def cpu(self, node_names):
+        for node_name in node_names:
+            manifest = new_pod_manifest(
+                image=IMAGE_LITMUX,
+                command=["stress-ng"],
+                args=['--cpu', str(get_node_cpu_cores(node_name)),
+                      '--cpu-load', str(NODE_CPU_LOAD_PERCENTAGE),
+                      '--timeout', str(NODE_STRESS_TIMEOUT_SECOND)],
+                node_name=node_name,
+                labels={'app': LABEL_STRESS_HELPER}
+            )
+
+            pod_name = manifest['metadata']['name']
+            logging(f"Creating cpu stress pod {pod_name} on {node_name}")
+            create_pod(manifest, is_wait_for_pod_running=True)
--- e2e/libs/workload/pod.py
+++ e2e/libs/workload/pod.py
@@ -1,6 +1,97 @@
+import time
+
 from kubernetes import client
 
+from robot.libraries.BuiltIn import BuiltIn
+
+from utility.utility import logging
+from utility.utility import generate_name
+
+
+IMAGE_BUSYBOX = 'busybox:1.34.0'
+IMAGE_LITMUX = 'litmuschaos/go-runner:latest'
+
+def new_pod_manifest(image="", command=[], args=[],
+                     claim_name="", node_name="", labels={}):
+    # Set default image and args
+    if image is None:
+        image = IMAGE_BUSYBOX
+        args = [
+            '/bin/sh', '-c',
+            'while true; do date; sleep 5; done'
+        ]
+
+    manifest = {
+        'apiVersion': 'v1',
+        'kind': 'Pod',
+        'metadata': {
+            'name': generate_name(),
+            'namespace': 'default',
+            'labels': labels
+        },
+        'spec': {
+            'nodeName': node_name,
+            'containers': [{
+                'image': image,
+                'imagePullPolicy': 'IfNotPresent',
+                'name': 'run',
+                'command': command,
+                'args': args,
+                'volumeMounts': [],
+            }],
+            'volumes': []
+        }
+    }
+
+    if claim_name != "":
+        manifest['spec']['volumes'].append({
+            'name': 'pod-data',
+            'persistentVolumeClaim': {
+                'claimName': claim_name
+            }
+        })
+
+        manifest['spec']['containers'][0]['volumeMounts'].append({
+            'name': 'pod-data',
+            'mountPath': '/data'
+        })
+
+    return manifest
+
+def create_pod(manifest, is_wait_for_pod_running=False):
+    core_api = client.CoreV1Api()
+
+    name = manifest['metadata']['name']
+    namespace = manifest['metadata']['namespace']
+
+    core_api.create_namespaced_pod(body=manifest, namespace=namespace)
+
+    if is_wait_for_pod_running:
+        wait_for_pod_status(name, 'Running', namespace=namespace)
+
+    return get_pod(name, namespace=namespace)
 
 def delete_pod(name, namespace='default'):
     core_api = client.CoreV1Api()
     core_api.delete_namespaced_pod(name=name, namespace=namespace)
+
+def get_pod(name, namespace='default'):
+    core_api = client.CoreV1Api()
+    return core_api.read_namespaced_pod(name=name, namespace=namespace)
+
+def wait_for_pod_status(name, status, namespace='default',
+                        retry_count=int(BuiltIn().get_variable_value("${RETRY_COUNT}")),
+                        retry_interval=int(BuiltIn().get_variable_value("${RETRY_INTERVAL}"))):
+    is_running = False
+    for i in range(retry_count):
+        pod = get_pod(name, namespace)
+
+        logging(f"Waiting for pod {name} status {status}, current status {pod.status.phase} ({i}) ...")
+
+        if pod.status.phase == status:
+            is_running = True
+            break
+
+        time.sleep(retry_interval)
+
+    assert is_running
