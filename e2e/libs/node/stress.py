c8e587e2f220c9fe62776690cb9d0672b75f8e35
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-10-27 13:03:47 +0800

    fix(negative): duplicated stress pod
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- e2e/libs/node/stress.py
+++ e2e/libs/node/stress.py
@@ -1,15 +1,19 @@
+from kubernetes.client.rest import ApiException
+
 from node.utility import get_node_cpu_cores
 
-from node.constant import LABEL_STRESS_HELPER
 from node.constant import NODE_STRESS_CPU_LOAD_PERCENTAGE
 from node.constant import NODE_STRESS_MEM_LOAD_PERCENTAGE
 from node.constant import NODE_STRESS_MEM_VM_WORKERS
 from node.constant import NODE_STRESS_TIMEOUT_SECOND
+from node.constant import STRESS_HELPER_LABEL
+from node.constant import STRESS_HELPER_POD_NAME_PREFIX
 
 from utility.utility import logging
 
 from workload.pod import create_pod
 from workload.pod import delete_pod
+from workload.pod import get_pod
 from workload.pod import new_pod_manifest
 from workload.workload import get_workload_pods
 
@@ -18,20 +22,36 @@ from workload.constant import IMAGE_LITMUX
 
 class Stress:
     def cleanup(self):
-        for pod in get_workload_pods(LABEL_STRESS_HELPER):
+        for pod in get_workload_pods(STRESS_HELPER_LABEL):
             logging(f"Cleaning up stress pod {pod.metadata.name}")
             delete_pod(pod.metadata.name, pod.metadata.namespace)
 
     def cpu(self, node_names):
         for node_name in node_names:
+            pod_name = f"{STRESS_HELPER_POD_NAME_PREFIX}{node_name}"
+
+            # If the helper pod creation is called inside of a test case loop,
+            # we need to check if the pod already running.
+            try:
+                pod = get_pod(pod_name)
+                if pod and pod.status.phase != "Running":
+                    logging(f"Deleting stress pod {pod_name} in phase {pod.status.phase}")
+                    delete_pod(pod_name)
+                elif pod:
+                    logging(f"Stress pod {pod_name} already running")
+                    continue
+            except ApiException as e:
+                assert e.status == 404
+
             manifest = new_pod_manifest(
+                pod_name=pod_name,
                 image=IMAGE_LITMUX,
                 command=["stress-ng"],
                 args=['--cpu', str(get_node_cpu_cores(node_name)),
                       '--cpu-load', str(NODE_STRESS_CPU_LOAD_PERCENTAGE),
                       '--timeout', str(NODE_STRESS_TIMEOUT_SECOND)],
                 node_name=node_name,
-                labels={'app': LABEL_STRESS_HELPER}
+                labels={'app': STRESS_HELPER_LABEL}
             )
 
             pod_name = manifest['metadata']['name']
@@ -40,14 +60,30 @@ class Stress:
 
     def memory(self, node_names):
         for node_name in node_names:
+            pod_name = f"{STRESS_HELPER_POD_NAME_PREFIX}{node_name}"
+
+            # If the helper pod creation is called inside of a test case loop,
+            # we need to check if the pod already running.
+            try:
+                pod = get_pod(pod_name)
+                if pod and pod.status.phase != "Running":
+                    logging(f"Deleting stress pod {pod_name} in phase {pod.status.phase}")
+                    delete_pod(pod_name)
+                elif pod:
+                    logging(f"Stress pod {pod_name} already running")
+                    continue
+            except ApiException as e:
+                assert e.status == 404
+
             manifest = new_pod_manifest(
+                pod_name=pod_name,
                 image=IMAGE_LITMUX,
                 command=["stress-ng"],
                 args=['--vm', str(NODE_STRESS_MEM_VM_WORKERS),
 					  '--vm-bytes', f"{NODE_STRESS_MEM_LOAD_PERCENTAGE}%",
 					  '--timeout', str(NODE_STRESS_TIMEOUT_SECOND)],
                 node_name=node_name,
-                labels={'app': LABEL_STRESS_HELPER}
+                labels={'app': STRESS_HELPER_LABEL}
             )
 
             pod_name = manifest['metadata']['name']
