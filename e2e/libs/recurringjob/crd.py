b2017d0cd6f8459d31562b8b20326105172ca9e5
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-11-27 13:27:13 +0800

    test(negative): recurring job crd
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- e2e/libs/recurringjob/crd.py
+++ e2e/libs/recurringjob/crd.py
@@ -1,5 +1,12 @@
+import time
+
+from kubernetes import client
+
 from recurringjob.base import Base
 from recurringjob.rest import Rest
+
+from recurringjob.constant import RETRY_COUNTS
+from recurringjob.constant import RETRY_INTERVAL
 
 from utility.utility import logging
 
@@ -8,17 +15,60 @@ class CRD(Base):
 
     def __init__(self):
         self.rest = Rest()
+        self.batch_v1_api = client.BatchV1Api()
+        self.obj_api = client.CustomObjectsApi()
+
+    def cleanup(self, recurringjob_names):
+        for recurringjob_name in recurringjob_names:
+            self.delete(recurringjob_name)
 
     def create(self, name, task, groups, cron, retain, concurrency, label):
-        logging("Delegating the create call to API because there is no CRD implementation")
-        return self.rest.create(name, task, groups, cron, retain, concurrency, label)
+        body = {
+            "apiVersion": "longhorn.io/v1beta2",
+            "kind": "RecurringJob",
+            "metadata": {"name": name},
+            "spec": {
+                "name": name,
+                "groups": groups,
+                "task": task,
+                "cron": cron,
+                "retain": retain,
+                "concurrency": concurrency,
+                "labels": label
+            }
+        }
+        self.obj_api.create_namespaced_custom_object(
+            group="longhorn.io",
+            version="v1beta2",
+            namespace="longhorn-system",
+            plural="recurringjobs",
+            body=body
+        )
 
-    def delete(self, job_name, volume_name):
-        logging("Delegating the delete call to API because there is no CRD implementation")
-        return self.rest.delete(job_name, volume_name)
+        return self.wait_for_cron_job_create(name)
+
+    def delete(self, recurringjob_name):
+        try:
+            self.obj_api.delete_namespaced_custom_object(
+                group="longhorn.io",
+                version="v1beta2",
+                namespace="longhorn-system",
+                plural="recurringjobs",
+                name=recurringjob_name,
+                body=client.V1DeleteOptions(),
+            )
+        except Exception as e:
+            logging(f"Failed to delete recurringjob {recurringjob_name}: {e}")
+            return False
 
     def get(self, name):
-        return self.rest.get(name)
+        return self.obj_api.get_namespaced_custom_object(
+            group="longhorn.io",
+            version="v1beta2",
+            namespace="longhorn-system",
+            plural="recurringjobs",
+            name=name,
+        )
 
     def add_to_volume(self, job_name, volume_name):
         logging("Delegating the add_to_volume call to API because there is no CRD implementation")
@@ -28,6 +78,15 @@ class CRD(Base):
         logging("Delegating the check_jobs_work call to API because there is no CRD implementation")
         return self.rest.check_jobs_work(volume_name)
 
-    def cleanup(self, volume_names):
-        logging("Delegating the cleanup call to API because there is no CRD implementation")
-        return self.rest.cleanup(volume_names)
+    def wait_for_cron_job_create(self, job_name):
+        is_created = False
+        for _ in range(RETRY_COUNTS):
+            job = self.batch_v1_api.list_namespaced_cron_job(
+                'longhorn-system',
+                label_selector=f"recurring-job.longhorn.io={job_name}"
+            )
+            if len(job.items) != 0:
+                is_created = True
+                break
+            time.sleep(RETRY_INTERVAL)
+        assert is_created
