ee41d3b9d9c61eaa57e2d535d7842c075fb9afab
Yang Chiu <yang.chiu@suse.com>
2024-01-01 20:13:32 +0800

    ci: add talos provisioning
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- pipelines/utilities/kubeconfig.sh
+++ pipelines/utilities/kubeconfig.sh
@@ -1,7 +1,13 @@
 set_kubeconfig(){
   # rke2, rke and k3s all support amd64
   # but only k3s supports arm64
-  if [[ "${LONGHORN_TEST_CLOUDPROVIDER}" == "harvester" ]]; then
+  if [[ "${DISTRO}" == "talos" ]]; then
+    export KUBECONFIG="${PWD}/test_framework/terraform/${LONGHORN_TEST_CLOUDPROVIDER}/${DISTRO}/kubeconfig"
+    until [ $(kubectl get node -o jsonpath='{.items[*].status.conditions}'  | jq '.[] | select(.type  == "Ready").status' | grep -ci true) -eq 4 ]; do
+      echo "waiting for talos cluster nodes to be running";
+      sleep 30;
+    done
+  elif [[ "${LONGHORN_TEST_CLOUDPROVIDER}" == "harvester" ]]; then
     export KUBECONFIG="${PWD}/test_framework/kube_config.yaml"
   elif [[ "${TF_VAR_arch}" == "amd64" ]]; then
     if [[ "${TF_VAR_k8s_distro_name}" == "rke" ]]; then
--- test_framework/scripts/kubeconfig.sh
+++ test_framework/scripts/kubeconfig.sh
@@ -1,5 +1,13 @@
 set_kubeconfig(){
-  if [[ "${TF_VAR_k8s_distro_name}" == "rke" ]]; then
+  if [[ "${DISTRO}" == "talos" ]]; then
+    export KUBECONFIG="test_framework/terraform/${LONGHORN_TEST_CLOUDPROVIDER}/${DISTRO}/kubeconfig"
+    until [ $(kubectl get node -o jsonpath='{.items[*].status.conditions}' | jq '.[] | select(.type  == "Ready").status' | grep -ci true) -eq 4 ]; do
+      echo "waiting for talos cluster nodes to be running";
+      sleep 30;
+    done
+  elif [[ "${LONGHORN_TEST_CLOUDPROVIDER}" == "harvester" ]]; then
+    export KUBECONFIG="test_framework/kube_config.yaml"
+  elif [[ "${TF_VAR_k8s_distro_name}" == "rke" ]]; then
     export KUBECONFIG="test_framework/kube_config_rke.yml"
   elif [[ "${TF_VAR_k8s_distro_name}" == "rke2" ]]; then
     export KUBECONFIG="test_framework/terraform/${LONGHORN_TEST_CLOUDPROVIDER}/${DISTRO}/rke2.yaml"
