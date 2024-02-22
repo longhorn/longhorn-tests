0f6a287965cb4c90c4aedd6450309a0999953f97
Chris <chris.chien@suse.com>
2024-02-22 20:02:53 +0800

    Use longhorn repo for backup store creation
    ref: 8004
    
    Signed-off-by: Chris <chris.chien@suse.com>
    (cherry picked from commit d4fcafdd52ba543b1c3d112d3a1fa211e7c7c1f8)
    
    # Conflicts:
    #	pipelines/utilities/install_backupstores.sh

--- test_framework/scripts/longhorn-setup.sh
+++ test_framework/scripts/longhorn-setup.sh
@@ -273,8 +273,8 @@ create_longhorn_namespace(){
 
 
 install_backupstores(){
-  MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/minio-backupstore.yaml"
-  NFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/nfs-backupstore.yaml"
+  MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/minio-backupstore.yaml"
+  NFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/nfs-backupstore.yaml"
   kubectl create -f ${MINIO_BACKUPSTORE_URL} \
                -f ${NFS_BACKUPSTORE_URL}
 }
