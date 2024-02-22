c9ebea8468db380d9189822794b7917b9215adc1
Chris <chris.chien@suse.com>
2024-02-22 20:02:53 +0800

    Use longhorn repo for backup store creation
    ref: 8004
    
    Signed-off-by: Chris <chris.chien@suse.com>
    (cherry picked from commit d4fcafdd52ba543b1c3d112d3a1fa211e7c7c1f8)
    
    # Conflicts:
    #	pipelines/utilities/install_backupstores.sh

---
+++ pipelines/utilities/install_backupstores.sh
@@ -0,0 +1,6 @@
+install_backupstores(){
+  MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/minio-backupstore.yaml"
+  NFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/backupstores/nfs-backupstore.yaml"
+  kubectl create -f ${MINIO_BACKUPSTORE_URL} \
+                 -f ${NFS_BACKUPSTORE_URL}
+}
