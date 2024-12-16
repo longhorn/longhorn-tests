ca8d0bbf0d4bd9824fc32c56513111a8702ff95b
Chin-Ya Huang <chin-ya.huang@suse.com>
2024-12-16 13:34:28 +0800

    chore(robot): cleanup
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- e2e/libs/backing_image/crd.py
+++ e2e/libs/backing_image/crd.py
@@ -1,16 +1,19 @@
-from kubernetes import client
+import time
+
 from datetime import datetime
+from kubernetes import client
+
 from backing_image.base import Base
 
 from utility.utility import logging
 from utility.utility import get_retry_count_and_interval
-import time
+
 
 class CRD(Base):
     def __init__(self):
         self.obj_api = client.CustomObjectsApi()
         self.retry_count, self.retry_interval = get_retry_count_and_interval()
-        
+
     def create(self, bi_name, source_type, url, expected_checksum):
         return NotImplemented
 
@@ -33,7 +36,7 @@ class CRD(Base):
 
     def cleanup_backing_images(self):
         return NotImplemented
-    
+
     def list_backing_image_manager(self):
         label_selector = 'longhorn.io/component=backing-image-manager'
         return self.obj_api.list_namespaced_custom_object(
@@ -42,7 +45,7 @@ class CRD(Base):
             namespace="longhorn-system",
             plural="backingimagemanagers",
             label_selector=label_selector)
-    
+
     def delete_backing_image_manager(self, name):
         logging(f"deleting backing image manager {name} ...")
         self.obj_api.delete_namespaced_custom_object(
@@ -56,7 +59,7 @@ class CRD(Base):
     def wait_all_backing_image_managers_running(self):
         for i in range(self.retry_count):
             all_running = True
-            backing_image_managers = self.list_backing_image_manager()            
+            backing_image_managers = self.list_backing_image_manager()
             for backing_image_manager in backing_image_managers["items"]:
                 current_state = backing_image_manager["status"]["currentState"]
                 name = backing_image_manager["metadata"]["name"]
@@ -70,7 +73,7 @@ class CRD(Base):
 
     def wait_backing_image_manager_restart(self, name, last_creation_time):
         for i in range(self.retry_count):
-            time.sleep(self.retry_interval)            
+            time.sleep(self.retry_interval)
             try:
                 backing_image_manager = self.obj_api.get_namespaced_custom_object(
                     group="longhorn.io",
@@ -82,7 +85,7 @@ class CRD(Base):
             except Exception as e:
                 logging(f"Finding backing image manager {name} failed with error {e}")
                 continue
-            
+
             creation_time = backing_image_manager["metadata"]["creationTimestamp"]
             fmt = "%Y-%m-%dT%H:%M:%SZ"
             if datetime.strptime(creation_time, fmt) > datetime.strptime(last_creation_time, fmt):
