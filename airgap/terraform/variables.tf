da668f9e0066b185acdc937302521a050495c392
Yang Chiu <yang.chiu@suse.com>
2025-02-04 22:04:54 +0800

    ci: migrate air-gap private registry from digitalocean to aws
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- airgap/terraform/variables.tf
+++ airgap/terraform/variables.tf
@@ -13,11 +13,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "do_token" {
-  type        = string
-  description = "DO TOKEN"
-}
-
 variable "os_distro_version" {
   type        = string
   default     = "20.04"
