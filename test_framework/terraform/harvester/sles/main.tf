22c0f3e11059a5fdae1d53b7e29f97fbe1ce503c
Chris Chien <chris.chien@suse.com>
2025-03-31 17:58:18 +0800

    ci: refine HAL pipeline flow
    longhorn/longhorn#10597
    
    Signed-off-by: Chris Chien <chris.chien@suse.com>
    (cherry picked from commit 91112fb26f52a6b0f62b6cec9803323891f8dd82)

--- test_framework/terraform/harvester/sles/main.tf
+++ test_framework/terraform/harvester/sles/main.tf
@@ -122,6 +122,12 @@ resource "rancher2_machine_config_v2" "e2e-machine-config-worker" {
 ssh_authorized_keys:
   - >-
     ${file(var.ssh_public_key_file_path)}
+write_files:
+  - path: "/tmp/SUSE_Trust_Root_encoded.crt"
+    content: >-
+      ${fileexists("/usr/local/share/ca-certificates/suse/SUSE_Trust_Root.crt")
+      ? filebase64("/usr/local/share/ca-certificates/suse/SUSE_Trust_Root.crt")
+      : ""}
 runcmd:
   - SUSEConnect -r ${var.registration_code}
   - zypper install -y qemu-guest-agent iptables open-iscsi nfs-client cryptsetup device-mapper
