f731a56a363b2ddc4403861a51acc8f3ae564a08
Roger Yao <roger.yao@suse.com>
2025-04-02 16:59:55 +0800

    ci: use upstream Talos AMI
    - Added talosctl installation
    - Updated `variables.tf` to change the default OS distribution version from 1.8.3 to 1.9.5.
    - Added a new resource in `main.tf` to upload the Talos schematic
    - Handle upgrades for control and worker nodes.
    
    ref: https://github.com/longhorn/longhorn/issues/9932
    
    Signed-off-by: Roger Yao <roger.yao@suse.com>

--- test_framework/terraform/aws/talos/data.tf
+++ test_framework/terraform/aws/talos/data.tf
@@ -2,7 +2,7 @@ data "aws_ami" "talos" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["talos-v${var.os_distro_version}-${var.arch}"]
+    values = ["talos-v${var.os_distro_version}-${var.aws_region}-${var.arch}"]
   }
   owners = [var.aws_ami_talos_account_number]
 }
