8072839d760495a2fe18cbea191423d81cc6cefa
Yang Chiu <yang.chiu@suse.com>
2025-04-11 10:06:59 +0800

    test(robot): migrate test_upgrade test case
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>
    (cherry picked from commit 9f5258dbcbf6c68a731070fd14fa648460918eec)

--- e2e/libs/instancemanager/instancemanager.py
+++ e2e/libs/instancemanager/instancemanager.py
@@ -1,4 +1,5 @@
 import time
+from kubernetes import client
 
 from node import Node
 
@@ -11,13 +12,13 @@ class InstanceManager:
 
     def __init__(self):
         self.node = Node()
+        self.retry_count, self.retry_interval = get_retry_count_and_interval()
 
     def wait_for_all_instance_manager_running(self):
         longhorn_client = get_longhorn_client()
         worker_nodes = self.node.list_node_names_by_role("worker")
 
-        retry_count, retry_interval = get_retry_count_and_interval()
-        for i in range(retry_count):
+        for i in range(self.retry_count):
             try:
                 instance_managers = longhorn_client.list_instance_manager()
                 instance_manager_map = {}
@@ -30,6 +31,20 @@ class InstanceManager:
                 logging(f"Getting instance manager state error: {e}")
 
             logging(f"Waiting for all instance manager running, retry ({i}) ...")
-            time.sleep(retry_interval)
+            time.sleep(self.retry_interval)
 
         assert len(instance_manager_map) == len(worker_nodes), f"expect all instance managers running, instance_managers = {instance_managers}, instance_manager_map = {instance_manager_map}"
+
+    def check_all_instance_managers_not_restart(self):
+
+        ims = get_longhorn_client().list_instance_manager()
+        v1_im_names = [im.name for im in ims if im.dataEngine == "v1"]
+        logging(f"Checking v1 instance managers {v1_im_names} didn't restart")
+
+        core_api = client.CoreV1Api()
+        for im_name in v1_im_names:
+            pod = core_api.read_namespaced_pod(name=im_name, namespace="longhorn-system")
+            if pod.status.container_statuses[0].restart_count != 0:
+                logging(f"Unexpected instance manager restart: {pod}")
+                time.sleep(self.retry_count)
+                assert False, f"Unexpected instance manager restart: {pod}"
--- e2e/libs/keywords/instancemanager_keywords.py
+++ e2e/libs/keywords/instancemanager_keywords.py
@@ -11,3 +11,6 @@ class instancemanager_keywords:
     def wait_for_all_instance_manager_running(self):
         logging(f'Waiting for all instance manager running')
         self.instancemanager.wait_for_all_instance_manager_running()
+
+    def check_all_instance_managers_not_restart(self):
+        self.instancemanager.check_all_instance_managers_not_restart()
