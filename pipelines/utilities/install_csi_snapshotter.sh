49b84fa3e1e38023dac353adfe8846166c91c898
Yang Chiu <yang.chiu@suse.com>
2025-07-23 09:39:04 +0800

    ci: install corresponding csi snapshotter based on longhorn branch
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- pipelines/utilities/install_csi_snapshotter.sh
+++ pipelines/utilities/install_csi_snapshotter.sh
@@ -1,13 +1,17 @@
 install_csi_snapshotter(){
-  CSI_SNAPSHOTTER_REPO_URL="https://github.com/kubernetes-csi/external-snapshotter.git"
-  CSI_SNAPSHOTTER_REPO_BRANCH="v6.2.1"
+  CSI_SNAPSHOTTER_REPO_URL="https://github.com/longhorn/csi-snapshotter.git"
   CSI_SNAPSHOTTER_REPO_DIR="${TMPDIR}/k8s-csi-external-snapshotter"
+
+  [[ "${LONGHORN_REPO_URI}" =~ https://([^/]+)/([^/]+)/([^/.]+)(.git)? ]]
+  wget "https://raw.githubusercontent.com/${BASH_REMATCH[2]}/${BASH_REMATCH[3]}/${LONGHORN_REPO_BRANCH}/deploy/longhorn-images.txt" -O "/tmp/longhorn-images.txt"
+  IFS=: read -ra IMAGE_TAG_PAIR <<< $(grep csi-snapshotter /tmp/longhorn-images.txt)
+  CSI_SNAPSHOTTER_REPO_BRANCH="${IMAGE_TAG_PAIR[1]}"
 
   git clone --single-branch \
             --branch "${CSI_SNAPSHOTTER_REPO_BRANCH}" \
             "${CSI_SNAPSHOTTER_REPO_URL}" \
             "${CSI_SNAPSHOTTER_REPO_DIR}"
 
-  kubectl apply -f "${CSI_SNAPSHOTTER_REPO_DIR}/client/config/crd" \
-                -f "${CSI_SNAPSHOTTER_REPO_DIR}/deploy/kubernetes/snapshot-controller"
+  kubectl apply -f ${CSI_SNAPSHOTTER_REPO_DIR}/client/config/crd \
+                -f ${CSI_SNAPSHOTTER_REPO_DIR}/deploy/kubernetes/snapshot-controller
 }
