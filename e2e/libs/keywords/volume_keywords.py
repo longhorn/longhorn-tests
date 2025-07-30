9e69d6c232b34aa5dccc4d72e54ab39cb0168475
Chris Chien <chris.chien@suse.com>
2025-07-30 21:43:37 +0800

    test(robot): automate test case Test Backing Image during Longhorn upgrade
    longhorn/longhorn-113112
    
    Signed-off-by: Chris Chien <chris.chien@suse.com>

--- e2e/libs/keywords/volume_keywords.py
+++ e2e/libs/keywords/volume_keywords.py
@@ -39,11 +39,11 @@ class volume_keywords:
         logging(f'Deleting volume {volume_name}')
         self.volume.delete(volume_name)
 
-    def attach_volume(self, volume_name, node_name=None):
+    def attach_volume(self, volume_name, node_name=None, wait_volume_attached=True):
         if not node_name:
             node_name = self.node.get_node_by_index(0)
         logging(f'Attaching volume {volume_name} to node {node_name}')
-        self.volume.attach(volume_name, node_name, disable_frontend=False)
+        self.volume.attach(volume_name, node_name, disable_frontend=False, wait_volume_attached=wait_volume_attached)
 
     def is_attached_to(self, volume_name, node_name):
         return self.volume.is_attached_to(volume_name, node_name)
