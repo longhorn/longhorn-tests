4a41a397b9900473b0257c6727521f2737c4068a
Yang Chiu <yang.chiu@suse.com>
2025-09-24 11:57:11 +0800

    test(robot): support --count option for robot test
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- e2e/run.sh
+++ e2e/run.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-robot -x junit.xml -P ./libs -d /tmp/test-report "$@" ./tests
+count=1
+args=()
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --count)
+      # Handle "--count N"
+      if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+        count="$2"
+        shift # skip the number N
+      fi
+      ;;
+    --count=*)
+      # Handle "--count=N"
+      count="${1#*=}"
+      ;;
+    *)
+      # Collect other args as options for robot)
+      args+=("$1")
+      ;;
+  esac
+  shift
+done
+
+# Build repeated ./tests
+tests=()
+for ((i=0; i<count; i++)); do
+  tests+=("./tests")
+done
+
+robot -x junit.xml -P ./libs -d /tmp/test-report "${args[@]}" "${tests[@]}"
