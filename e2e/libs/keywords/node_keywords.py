627cc717c3c2ac3a61fbc9f02bcbbfb689494b27
Yang Chiu <yang.chiu@suse.com>
2025-10-07 16:49:02 +0800

    test(robot): add Test No Transient Error In Engine Status During Eviction test case
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>
    (cherry picked from commit 391e6a8ef4136374c579cb566f5bc6e6772d8794)

--- e2e/libs/keywords/node_keywords.py
+++ e2e/libs/keywords/node_keywords.py
@@ -66,13 +66,33 @@ class node_keywords:
     def enable_disk(self, node_name, disk_name):
         self.node.set_disk_scheduling(node_name, disk_name, allowScheduling=True)
 
+    def request_eviction_on_default_disk(self, node_name):
+        self.node.set_default_disk_eviction_requested(node_name, evictionRequested=True)
+
+    def cancel_eviction_on_default_disk(self, node_name):
+        self.node.set_default_disk_eviction_requested(node_name, evictionRequested=False)
+
+    def request_eviction_on_disk(self, node_name, disk_name):
+        self.node.set_disk_eviction_requested(node_name, disk_name, evictionRequested=True)
+
+    def cancel_eviction_on_disk(self, node_name, disk_name):
+        self.node.set_disk_eviction_requested(node_name, disk_name, evictionRequested=False)
+
     def disable_node_scheduling(self, node_name):
         self.node.set_node_scheduling(node_name, allowScheduling=False)
 
     def enable_node_scheduling(self, node_name):
         self.node.set_node_scheduling(node_name, allowScheduling=True)
 
-    def reset_node_schedule(self):
+    def reset_disk_eviction_and_scheduling(self):
+        nodes = self.node.list_node_names_by_role("worker")
+        for node_name in nodes:
+            disks = self.node.list_disks_by_node_name(node_name)
+            for disk_name in disks:
+                self.node.set_disk_eviction_requested(node_name, disk_name, evictionRequested=False)
+                self.node.set_disk_scheduling(node_name, disk_name, allowScheduling=True)
+
+    def reset_node_scheduling(self):
         nodes = self.node.list_node_names_by_role("worker")
         for node_name in nodes:
             self.enable_node_scheduling(node_name)
