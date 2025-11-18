a37fa00973f9cce6a6b57d8eb564cf0301d17706
Chris Chien <chris.chien@suse.com>
2025-11-18 16:35:50 +0800

    test(ci): update azurite yaml path
    Signed-off-by: Chris Chien <chris.chien@suse.com>
    (cherry picked from commit 561b1eb56e4e0844e60792d2a1fa143a79f5b3b4)

--- pipelines/utilities/install_backupstores.sh
+++ pipelines/utilities/install_backupstores.sh
@@ -6,7 +6,7 @@ install_backupstores(){
   MINIO_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/minio-backupstore.yaml"
   NFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/nfs-backupstore.yaml"
   CIFS_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/cifs-backupstore.yaml"
-  AZURITE_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/deploy/backupstores/azurite-backupstore.yaml"
+  AZURITE_BACKUPSTORE_URL="https://raw.githubusercontent.com/longhorn/longhorn-tests/master/manager/integration/deploy/backupstores/azurite-backupstore.yaml"
   kubectl apply -f ${MINIO_BACKUPSTORE_URL} \
                  -f ${NFS_BACKUPSTORE_URL} \
                  -f ${CIFS_BACKUPSTORE_URL} \
