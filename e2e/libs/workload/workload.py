d33ef92be39646619ff8bc7b6fcaed1724115fdf
Yang Chiu <yang.chiu@suse.com>
2025-11-18 16:29:14 +0800

    test(robot): add test case Test Delete Instance Manager Of Single Replica Volume
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- e2e/libs/workload/workload.py
+++ e2e/libs/workload/workload.py
@@ -5,14 +5,13 @@ import json
 from kubernetes import client
 from kubernetes.stream import stream
 
-from network.constant import CNI_NETWORK_STATUS_ANNOTATION
-
 from utility.utility import get_retry_count_and_interval
 from utility.utility import logging
 from utility.utility import list_namespaced_pod
 
 from workload.constant import WAIT_FOR_POD_STABLE_MAX_RETRY
 from workload.constant import WAIT_FOR_POD_KEPT_IN_STATE_TIME
+from workload.constant import CNI_NETWORK_STATUS_ANNOTATION
 from workload.pod import is_pod_terminated_by_kubelet
 from workload.pod import wait_for_pod_status
 
