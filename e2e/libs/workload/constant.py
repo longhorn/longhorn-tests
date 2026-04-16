60444b5757c37068407d5bd31354251a3726bd76
Yang Chiu <yang.chiu@suse.com>
2026-04-17 07:09:55 +0800

    test(robot): add test case Test Large Volume Replica Rebuilding Resilience
    (1) add test case Test Large Volume Replica Rebuilding Resilience
    (2) merge disconnect_node_network and disconnect_node_network_without_waiting_completion into one function
    (3) use prebuilt image to fix apt-get update and install may be slow and flaky
    
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>
    (cherry picked from commit d164856dc69f8e36bad9dfacb4d8e2f82f65407a)

--- e2e/libs/workload/constant.py
+++ e2e/libs/workload/constant.py
@@ -1,6 +1,7 @@
 IMAGE_BUSYBOX = 'busybox:1.34.0'
 IMAGE_LITMUX = 'litmuschaos/go-runner:3.27.0'
 IMAGE_UBUNTU = 'ubuntu:16.04'
+IMAGE_NETWORK_TEST = 'nicolaka/netshoot:v0.15'
 
 WAIT_FOR_POD_STABLE_MAX_RETRY = 180
 WAIT_FOR_POD_KEPT_IN_STATE_TIME = 30
