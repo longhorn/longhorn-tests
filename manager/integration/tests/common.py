af3320a3ce66cd0ec1cb26f9b474e4b594519b86
Chris Chien <chris.chien@suse.com>
2026-05-08 14:45:30 +0800

    test(pytest): update cleanup_storage_class for GKE
    longhorn/longhorn#13064
    
    Signed-off-by: Chris Chien <chris.chien@suse.com>
    (cherry picked from commit aa8a8324fab5ce04942bd5e8ed39c09c39502aab)

--- manager/integration/tests/common.py
+++ manager/integration/tests/common.py
@@ -4517,7 +4517,8 @@ def cleanup_storage_class():
                        "premium-rwo", "standard-rwo", "standard",
                        "azurefile-csi", "azurefile-csi-premium",
                        "azurefile-premium", "managed", "managed-csi",
-                       "managed-csi-premium", "managed-premium"]
+                       "managed-csi-premium", "managed-premium",
+                       "dynamic-rwo"]
     api = get_storage_api_client()
     ret = api.list_storage_class()
     for sc in ret.items:
