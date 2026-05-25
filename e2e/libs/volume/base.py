bb41410eb193188098c8405f504b1ff6761f532c
Yang Chiu <yang.chiu@suse.com>
2026-05-25 13:24:24 +0800

    test: fix upgrade engine image test when installing via helm chart
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>
    (cherry picked from commit a5c877bdad27dc4afb0f39dbc8104f3bf4e4c6a2)

--- e2e/libs/volume/base.py
+++ e2e/libs/volume/base.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 import time
 
+from engine import Engine
+from engine_image import EngineImage
+
 from persistentvolume.persistentvolume import PersistentVolume
 from persistentvolumeclaim.persistentvolumeclaim import PersistentVolumeClaim
 from utility.utility import set_annotation
@@ -20,6 +23,8 @@ class Base(ABC):
         self.retry_count, self.retry_interval = get_retry_count_and_interval()
         self.pv = PersistentVolume()
         self.pvc = PersistentVolumeClaim()
+        self.engine = Engine()
+        self.engineimage = EngineImage()
 
     def _get_volume_size(self, volume):
         # CRD returns dict-like payloads, REST returns object-like payloads.
