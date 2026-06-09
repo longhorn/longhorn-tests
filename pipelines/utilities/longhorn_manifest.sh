a67024979c5caf2d0a9a37a9c8fc03d36a79c81f
David Cheng <david.cheng@suse.com>
2026-06-09 15:02:06 +0800

    test(robot): add v2 live upgrade new instance manager e2e coverage
    Longhorn: 9104
    
    Signed-off-by: David Cheng <david.cheng@suse.com>
    (cherry picked from commit 36bfec263b27aa681cbc48b093b2e3a9f18d578b)

--- pipelines/utilities/longhorn_manifest.sh
+++ pipelines/utilities/longhorn_manifest.sh
@@ -131,6 +131,12 @@ uninstall_longhorn(){
 
   get_longhorn_repo "${UNINSTALL_VERSION}"
 
+  # replace longhorn-manager image in uninstall manifest if custom image is specified
+  if [[ ! -z ${CUSTOM_LONGHORN_MANAGER_IMAGE} ]]; then
+    LONGHORN_MANAGER_IMAGE=`grep -io "longhornio\/longhorn-manager:.*$" "${LONGHORN_UNINSTALL_MANIFEST_PATH}"| head -1 | sed -e 's/^"//' -e 's/"$//'`
+    sed -i 's#'${LONGHORN_MANAGER_IMAGE}'#'${CUSTOM_LONGHORN_MANAGER_IMAGE}'#' "${LONGHORN_UNINSTALL_MANIFEST_PATH}"
+  fi
+
   sed -i "s/longhorn-system/${LONGHORN_NAMESPACE}/g" "${LONGHORN_UNINSTALL_MANIFEST_PATH}"
   kubectl create -f "${LONGHORN_UNINSTALL_MANIFEST_PATH}"
   kubectl patch job longhorn-uninstall -n "${LONGHORN_NAMESPACE}" --type=json -p='[{"op":"remove","path":"/spec/activeDeadlineSeconds"}]'
