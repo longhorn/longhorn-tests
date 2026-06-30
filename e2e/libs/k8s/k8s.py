81c0e67d834f125344cb427071b583e9a79f7639
Yang Chiu <yang.chiu@suse.com>
2026-06-30 10:16:57 +0800

    test(robot): add test case Node Down Between Migration And Confirmation
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>
    (cherry picked from commit 5d6051249d7f45f63d0f630de0c5de2db48ffc3c)

--- e2e/libs/k8s/k8s.py
+++ e2e/libs/k8s/k8s.py
@@ -158,6 +158,17 @@ def wait_for_node_ready(node_name):
 
     assert False, f"Node {node_name} is not ready after waiting for {retry_count * retry_interval} seconds."
 
+def wait_for_node_down(node_name):
+    retry_count, retry_interval = get_retry_count_and_interval()
+    for i in range(retry_count):
+        if not is_node_ready(node_name):
+            logging(f"Node {node_name} is down.")
+            return
+        logging(f"Waiting for node {node_name} down ... ({i})")
+        time.sleep(retry_interval)
+
+    assert False, f"Node {node_name} is still ready after waiting for {retry_count * retry_interval} seconds."
+
 def check_node_cordoned(node_name):
     api = client.CoreV1Api()
     node = api.read_node(node_name)
