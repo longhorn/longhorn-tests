8e2e7a5050fa52b6fe9e75f39d21c209f075824d
Chris Chien <chris.chien@suse.com>
2026-08-05 09:31:38 +0800

    test(robot): add missing parameter to verify_pod_log_after_time_not_contains
    Signed-off-by: Chris Chien <chris.chien@suse.com>
    (cherry picked from commit 31180dc8d8967bf5e27cbfbfbc6f087d22037b6e)

--- e2e/libs/k8s/k8s.py
+++ e2e/libs/k8s/k8s.py
@@ -334,7 +334,7 @@ def verify_pod_log_after_time_contains(pod_name, expect_log, test_start_time, na
 
     assert expect_log in pod_log, f"Expected log '{expect_log}' was not found in pod '{pod_name}' logs"
 
-def verify_pod_log_after_time_not_contains(pod_name, unexpected_log, test_start_time, namespace):
+def verify_pod_log_after_time_not_contains(pod_name, unexpected_log, test_start_time, namespace, container=None):
     # Convert test_start_time to UTC and format it for kubectl --since-time use
     test_start_time = test_start_time.astimezone(timezone.utc)
     test_start_time = test_start_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
@@ -344,6 +344,9 @@ def verify_pod_log_after_time_not_contains(pod_name, unexpected_log, test_start_
         "-n", namespace,
         f"--since-time={test_start_time}"
     ]
+
+    if container:
+        exec_cmd += ["-c", container]
 
     pod_log = subprocess_exec_cmd(exec_cmd)
     logging(f"logs in pod {pod_name} after {test_start_time}:\n {pod_log}")
