77358011632aa8f4a3e8fba1b95f062f0e586e7e
Chris Chien <chris.chien@suse.com>
2026-08-06 10:52:42 +0800

    test(robot): add refresh_public_ip_mapping in test setup
    longhorn/longhorn#13661
    
    Signed-off-by: Chris Chien <chris.chien@suse.com>

--- test_framework/terraform/aws/ubuntu/output.tf
+++ test_framework/terraform/aws/ubuntu/output.tf
@@ -31,25 +31,6 @@ output "instance_mapping" {
   )
 }
 
-output "public_ip_mapping" {
-  value = jsonencode(
-    concat(
-      [
-        for controlplane_instance in aws_instance.lh_aws_instance_controlplane : {
-          "name" = controlplane_instance.private_dns,
-          "ip"   = controlplane_instance.public_ip
-        }
-      ],
-      [
-        for worker_instance in aws_instance.lh_aws_instance_worker : {
-          "name" = worker_instance.private_dns,
-          "ip"   = worker_instance.public_ip
-        }
-      ]
-    )
-  )
-}
-
 output "controlplane_public_ip" {
   depends_on = [
     aws_eip.lh_aws_eip_controlplane
